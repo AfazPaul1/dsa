7d6427042622b46ce0685e3dd5b9a88480273c3d
AfazPaul1 <afazpaul@gmail.com>
2025-06-05 17:14:30 +0530

    striver

--- src/Highest_Occurring_Element_in_an_Array.ts
+++ src/Highest_Occurring_Element_in_an_Array.ts
@@ -1,23 +1,19 @@
 function mostFrequestElement(){
-    const arr: number[] =  [4, 4, 5, 5, 6]
+    const arr: number[] =  [4, 4, 5, 5, 5, 6]
     const hash: Map<number, number> = new Map()
     for (const ele of arr) {
         hash.set(ele, (hash.get(ele) || 0) + 1)
     }
-    const highEle: Record<string, number> = {
-        "keyHigh":0,
-        "valueHigh":0
-    }
+    let keyHigh = 0
+    let valueHigh = 0
+
     for (const [key, value] of hash) {
-        if (value === highEle.valueHigh && key < highEle.keyHigh) {
-            highEle.keyHigh = key 
-            highEle.valueHigh=value
-        } else if (value > highEle.valueHigh){
-            highEle.keyHigh = key 
-            highEle.valueHigh=value
+        if (value > valueHigh || value === valueHigh && key < keyHigh) {
+            keyHigh = key 
+            valueHigh=value
         }
     }
-    console.log(highEle);
+    console.log(keyHigh, valueHigh);
     
 }
 
