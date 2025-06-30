06481937bf4fd8973fb8a70e32bbc2b9596c8d92
AfazPaul1 <afazpaul@gmail.com>
2025-06-30 17:28:48 +0530

    update

--- src/Unique_Numbers.ts
+++ src/Unique_Numbers.ts
@@ -1,36 +1,10 @@
-//this would not work for a 3 digit number
-// function uniqueNumbers(L:number, R:number){
-//     let cur = L
-//     const list = []
-//     while ( cur <=R ) {
-//        let some = cur.toString().split("")
-//        console.log(some);
-       
-//        if(some[0] !== some[1]){
-//         list.push(cur)
-        
-//        }
-//        cur++
-//     }
-//     return list
-// }
-// console.log(uniqueNumbers(10, 20));
 function uniqueNumbers(L:number, R:number){
     let cur = L
     const list = []
     while ( cur <=R ) {
        let some = cur.toString().split("")
-       console.log(some);
-       
-       const ite = new Map()
-        for (const ele of some) {
-            ite.set(ele, (ite.get(ele) || 0) + 1)
-        }
-            
-        if (some.length === [...(ite.keys())].length) {
-            list.push(cur)
-        }
-       
+       //map was not necessary for just a  uniqueness check we only needed to know whether a digit has occured before
+       if((new Set(some)).size === some.length) list.push(cur)
        cur++
     }
     return list
