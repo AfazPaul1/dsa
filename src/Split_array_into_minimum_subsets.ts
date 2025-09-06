ca82e1026215eb8e84fa1e4171c5e128df3dd876
AfazPaul1 <afazpaul@gmail.com>
2025-09-06 11:10:34 +0530

    added check which avoids empty insertions

--- src/Split_array_into_minimum_subsets.ts
+++ src/Split_array_into_minimum_subsets.ts
@@ -2,6 +2,7 @@ function minSubsets(arr: number[]) {
     const arr1= new Set(arr)
     let final = []
     for (const ele of arr) {
+        console.log(arr)
         if(!arr1.has(ele - 1)) {
             let tempSubset = []
             //console.log(ele);
@@ -12,13 +13,15 @@ function minSubsets(arr: number[]) {
                 arr1.delete(newEle)
                 newEle++
             }
-            final.push(tempSubset)
+            if (tempSubset.length !== 0) {
+                final.push(tempSubset)
+            }
         }
     }
     return final
 }
-// const sub1 = minSubsets([102, 56, 5, 6, 100, 58, 101, 57, 7, 103, 59])
-// console.log(sub1);
+ const sub1 = minSubsets([102, 56, 5, 6, 100, 58, 101, 57, 7, 103, 59])
+ console.log(sub1);
 
 //const hash = new Map()
     // for (const ele of arr) {
@@ -28,18 +31,18 @@ function minSubsets(arr: number[]) {
     // for (const ele of arr) {
     //     hash.set(ele, hash.has(ele -1)?  (hash.get(ele-1)).push(ele): hash.set(ele, [ele]) )
     // }
-// Test case
-const largeArray = [];
-for (let i = 0; i < 100000; i++) { // A moderately large array for testing
-    largeArray.push(Math.floor(Math.random() * 200000) + 1); // Random numbers
-}
-// Add some consecutive sequences to ensure interesting test data
-largeArray.push(1, 2, 3, 4, 5);
-largeArray.push(1000000, 1000001, 1000002, 1000003);
+// // Test case
+// const largeArray = [];
+// for (let i = 0; i < 100000; i++) { // A moderately large array for testing
+//     largeArray.push(Math.floor(Math.random() * 200000) + 1); // Random numbers
+// }
+// // Add some consecutive sequences to ensure interesting test data
+// largeArray.push(1, 2, 3, 4, 5);
+// largeArray.push(1000000, 1000001, 1000002, 1000003);
 
 
-console.time("minSubsets execution time"); // Start the timer
-const result = minSubsets(largeArray);
-console.timeEnd("minSubsets execution time"); // End the timer and print the duration
-// console.log(result); // You might want to comment this out for very large results
+// console.time("minSubsets execution time"); // Start the timer
+// const result = minSubsets(largeArray);
+// console.timeEnd("minSubsets execution time"); // End the timer and print the duration
+// // console.log(result); // You might want to comment this out for very large results
 
