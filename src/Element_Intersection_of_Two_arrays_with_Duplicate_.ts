b4d2cf1cca90a277300745962c2e929a1e166f51
AfazPaul1 <afazpaul@gmail.com>
2025-09-09 16:54:32 +0530

    set with custom intersection

--- src/Element_Intersection_of_Two_arrays_with_Duplicate_.ts
+++ src/Element_Intersection_of_Two_arrays_with_Duplicate_.ts
@@ -1,32 +1,20 @@
 function intersect(a:number[], b:number[]) {
-    //i could probably sort both arrays and then use 2 pointer to sole this
-    //but sorting adds overhead
-    //lets first try it
-    const newa = new Set(a.sort((a,b) => a-b))
-    const newb = new Set(b.sort((a,b) => a-b))
-    console.log(newa.intersection(newb)); //only esnext not in es2023
-    //console.log(newa,newb, odds);
-    //const res = newa.intersection(newb)
-    //didnt use a Set initially
-    //i dont think this accessing with index works with set
-    //probably would have to use a forof instead of a general index
-    // for (let i = 0; i < newa.size; i++) {
-    //     console.log("outer loop", i, newa[i]);
-        
-    //     for (let j = 0; j < b.length; j++) {
-    //         console.log("inner loop", j, b[j]);
-            
-    //         if (a[i] === b[j]) {
-    //             res.push(a[i])
-    //             i++;
-    //             j++
-    //             break
-    //         } else {
-    //             j++
-    //         }
-    //     }
-    // }
-    return newa.intersection(newb)
-}
+    const newa = new Set(a)
+    const newb = new Set(b)
+    const iterator = newa.size < newb.size? newa.keys() : newb.keys()
+    const res:number[] = []
+    const other = newa.size < newb.size? newb : newa
 
+    console.log(newa, newb, iterator, res);
+    for (const ele of iterator) { 
+        if(other.has(ele)) {
+            res.push(ele)
+        }
+    }
+    return res
+    //The time saved by avoiding a nested loop (O(A * B)) far exceeds the time spent on creating the Set (O(N)).
+    //so creating the set first is ok
+}
+console.time("minSubsets execution time");
 console.log(intersect([1, 2, 1, 3, 1], [3, 1, 3, 4, 1]));
+console.timeEnd("minSubsets execution time");
