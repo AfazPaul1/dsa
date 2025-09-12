f7d72e53f542ca254b7c89b374b6bc0ff0e12cdd
AfazPaul1 <afazpaul@gmail.com>
2025-09-12 16:51:02 +0530

    IG THIS IS OK

--- src/Distinct_Substrings.ts
+++ src/Distinct_Substrings.ts
@@ -1,14 +1,13 @@
 function distinctSubstring(s:string):number {
-
-    let numberOfDistinctSubstring:number=0
     const hash = new Set()
     for (let i = 0; i < s.length; i++){
-        console.log(i);
-        hash.add(`${s[i] + s[i+1]}`)
+        if (s[i+1]) hash.add(`${s[i] + s[i+1]}`)
     }
     console.log(hash);
     
-    return numberOfDistinctSubstring
+    return hash.size
 }
-console.log(distinctSubstring("ABCAB"));
-//Set(4) { 'AB', 'BC', 'CA', 'Bundefined' }
+console.log(distinctSubstring("XYZ"));
+//Set(2) { 'XY', 'YZ' }
+console.log(distinctSubstring("ABCABABA"));
+//Set(4) { 'AB', 'BC', 'CA', 'BA' }
