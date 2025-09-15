aff072b20073386d721e99aac343b43a3f8836c9
AfazPaul1 <afazpaul@gmail.com>
2025-09-15 14:31:25 +0530

    this doesnt work either cause this ll only catch its first. though due to collision it was inserted somewhere else

--- src/Quadratic_Probing_in_Hashing.ts
+++ src/Quadratic_Probing_in_Hashing.ts
@@ -3,24 +3,28 @@ function quadraticProbing(arr:number[], m:number) {
 
         function modularHash(key:number) {
             let hash = key%m
-            
+            console.log(table[hash], key);
+            if (table[hash] === key) {  
+                return
+                console.log(table[hash], key);
+            }
             if (table[hash] === -1) {
                 table[hash] = key
             } else {
                 //added i
                 let i = 0
                 let calcHash = hash
-                console.log("collision");
+                //console.log("collision");
                 
-                console.log(`index: ${i} key: ${key} orginalHash:${hash}`);
+                //console.log(`index: ${i} key: ${key} orginalHash:${hash}`);
                 while (table[calcHash] !== -1) {
-                    console.log("inside while");
-                    console.log("i: " + i + " failed hash: " + hash);
+                    //console.log("inside while");
+                    //console.log("i: " + i + " failed hash: " + hash);
                     i++
-                    console.log(i);
+                    //console.log(i);
                     calcHash = (hash+(i**2))%m
                 }
-                console.log("finalHash " + hash);
+                //console.log("finalHash " + hash);
                 table[calcHash] = key
             }
             return hash
@@ -34,3 +38,8 @@ function quadraticProbing(arr:number[], m:number) {
 console.log(quadraticProbing([85, 50, 19, 38, 66, 47], 19));
 //expected
 //[19, 38, -1, -1, -1, -1, -1, -1, -1, 85, 66, -1, 50, 47, -1, -1, -1, -1, -1]
+console.log(quadraticProbing([223, 874, 874], 7));
+// Your Code's output is: 
+// [874, -1, -1, 874, -1, -1, 223]
+// It's Correct output is: 
+// [874, -1, -1, -1, -1, -1, 223]
