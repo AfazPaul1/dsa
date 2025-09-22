06cc40cde7aaa8c9643af91f9e7b0cf0db62201b
AfazPaul1 <afazpaul@gmail.com>
2025-09-22 18:50:26 +0530

    same time not much difference?  maybe cause of array? could try map

--- src/recursion/fib.ts
+++ src/recursion/fib.ts
@@ -1,29 +1,31 @@
-function fib(n:number): number{
+function fib(n:number, cache:number[]): number{
+    if (cache[n] !== undefined) {
+        return cache[n]
+    }
     if(n === 0 ) return 0
     else if ( n === 1) return 1
     else {
-        return fib(n-1) + fib(n-2)
+        console.log(n);
+        
+        let result =  fib(n-1, cache) + fib(n-2, cache)
+        cache[n] = result
+        return result
     }
 }
-console.log(fib(7));
+console.time()
+//console.log(fib(30));
+//832040
+//default: 9.21ms
+console.log(fib(7, []));
+//102334155
+//default: 1.030s
+console.timeEnd()
+// λ npx tsx fib.ts
 // 7
 // 6
 // 5
 // 4
 // 3
 // 2
-// 2
-// 3
-// 2
-// 4
-// 3
-// 2
-// 2
-// 5
-// 4
-// 3
-// 2
-// 2
-// 3
-// 2
-// undefined
+// 13
+// default: 1.526ms
