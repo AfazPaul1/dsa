8b3671ea698545059d9d0adab36fe0455678319b
AfazPaul1 <afazpaul@gmail.com>
2025-09-23 23:18:46 +0530

    this works too. this was his initial approach

--- src/recursion/reverse.ts
+++ src/recursion/reverse.ts
@@ -1,15 +1,15 @@
 function reverse(arr:number[], i, n) {
     //console.log(i, i>n/2, n/2);
     
-    if(i >= n-1) return arr
-    arr = swap(arr, i, n-1)
-    return reverse(arr, i+1, n-1)
+    if(i >= n/2) return arr
+    arr = swap(arr, i, n-i-1)
+    return reverse(arr, i+1, n)
 }
 //console.log(reverse([1,2,3,4,5], 0, 5));
 //[ 5, 4, 3, 2, 1 ] 
 //this works for now
 //lets check for bigger array
-console.log(reverse([1,2,3,4,5, 6, 7, 8, 9, 10, 11], 0, 11));
+//console.log(reverse([1,2,3,4,5, 6, 7, 8, 9, 10, 11], 0, 11));
 //[12, 11, 9, 6,  5, 4,  7, 8, 3, 10, 2,  1]
 //works
 console.log(reverse([1,2,3,4,5, 6, 7, 8, 9, 10, 11, 12], 0, 12));
