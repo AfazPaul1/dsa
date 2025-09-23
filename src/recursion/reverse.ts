302be90fb7d1ca37faf1d6605a04ffe8c52c9e30
AfazPaul1 <afazpaul@gmail.com>
2025-09-23 19:15:15 +0530

    so yea i shoun't allow for the next swap which uses n-1 so that is what i should be preparing against. also added equal to cases for odd length

--- src/recursion/reverse.ts
+++ src/recursion/reverse.ts
@@ -1,7 +1,7 @@
 function reverse(arr:number[], i, n) {
     //console.log(i, i>n/2, n/2);
     
-    if(i > n) return arr
+    if(i >= n-1) return arr
     arr = swap(arr, i, n-1)
     return reverse(arr, i+1, n-1)
 }
@@ -10,13 +10,15 @@ function reverse(arr:number[], i, n) {
 //this works for now
 //lets check for bigger array
 console.log(reverse([1,2,3,4,5, 6, 7, 8, 9, 10, 11], 0, 11));
-//[12, 11, 9, 6,  5,4,  7, 8, 3, 10,2,  1]
-//this doesnt work
+//[12, 11, 9, 6,  5, 4,  7, 8, 3, 10, 2,  1]
+//works
+console.log(reverse([1,2,3,4,5, 6, 7, 8, 9, 10, 11, 12], 0, 12));
+//even case works too
 function swap(arr, i, x) {
     console.log(i, x);
     
     [arr[i], arr[x]] = [arr[x], arr[i]]
-    console.log(arr);
+    //console.log(arr);
     
     return arr
 }
