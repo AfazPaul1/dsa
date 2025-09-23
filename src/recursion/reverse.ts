748ea8ba071317c31736a3d109ab902c29e48c5f
AfazPaul1 <afazpaul@gmail.com>
2025-09-23 18:48:26 +0530

    its tracking the second pointer using n thats messing it up. its failing the base case and returning early cause the n keeps decreasing. maybe we can change the base case? or i should stop messing with n

--- src/recursion/reverse.ts
+++ src/recursion/reverse.ts
@@ -1,11 +1,23 @@
 function reverse(arr:number[], i, n) {
+    //console.log(i, i>n/2, n/2);
+    
     if(i > n/2) return arr
     arr = swap(arr, i, n-1)
-    return reverse(arr, i++, n-i-1)
+    return reverse(arr, i+1, n-1)
 }
-console.log(reverse([1,2,3,4,5], 0, 5));
+//console.log(reverse([1,2,3,4,5], 0, 5));
+//[ 5, 4, 3, 2, 1 ] 
+//this works for now
+//lets check for bigger array
+console.log(reverse([1,2,3,4,5, 6, 7, 8, 9, 10, 11], 0, 11));
+//[12, 11, 9, 6,  5,4,  7, 8, 3, 10,2,  1]
+//this doesnt work
 function swap(arr, i, x) {
+    console.log(i, x);
+    
     [arr[i], arr[x]] = [arr[x], arr[i]]
+    console.log(arr);
+    
     return arr
 }
 //λ npx tsx reverse.ts
