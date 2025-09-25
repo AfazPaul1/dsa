97aad9300cb7d0e39c15cf18e3bd47d1ed59a0e6
AfazPaul1 <afazpaul@gmail.com>
2025-09-25 17:03:57 +0530

    works

---
+++ src/recursion/subsequences.ts
@@ -0,0 +1,30 @@
+function subsequences(arr, subseq, n, i) {
+    if (i >= n) {
+        //console.log(subseq);
+        return
+    }
+    subseq.push(arr[i])
+    subsequences(arr, subseq, n, i+1)
+    console.log("before", subseq, i);
+    
+    subseq.splice(i-1, 1)
+    console.log("after", subseq, i);
+    
+    subsequences(arr, subseq, n, i+1)
+}
+console.log(subsequences([3,1,2], [], 3, 0));
+// before [ 3, 1, 2 ] 2
+// after [ 3, 2 ] 2
+// before [ 3, 2 ] 1
+// after [ 2 ] 1
+// before [ 2, 2 ] 2
+// after [ 2 ] 2
+// before [ 2 ] 0
+// after [] 0
+// before [ 1, 2 ] 2
+// after [ 1 ] 2
+// before [ 1 ] 1
+// after [] 1
+// before [ 2 ] 2
+// after [ 2 ] 2
+// undefined
