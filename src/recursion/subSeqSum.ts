bedf52f166b45877105b0d19c03f2918161eda66
AfazPaul1 <afazpaul@gmail.com>
2025-09-25 20:43:14 +0530

    only the subsequences whose sum matches the given sum

--- src/recursion/subSeqSum.ts
+++ src/recursion/subSeqSum.ts
@@ -1,14 +1,14 @@
-function subSeqSum(arr:number[], seq:number[], n:number, i:number, sum:number) {
+function subSeqSum(arr:number[], seq:number[], n:number, i:number, sum:number, gnSum) {
     if (i >= n) {
-        console.log(seq, sum);
+        if(sum === gnSum) console.log(seq, sum);
         return
     }
     seq.push(arr[i])
     sum+=arr[i]
-    subSeqSum(arr, seq, n, i+1, sum)
+    subSeqSum(arr, seq, n, i+1, sum, gnSum)
     seq.pop()
     sum-=arr[i]
-    subSeqSum(arr, seq, n, i+1, sum)
+    subSeqSum(arr, seq, n, i+1, sum, gnSum)
 
 }
-console.log(subSeqSum([1, 2, 1], [], 3, 0, 0));
+console.log(subSeqSum([1, 2, 1], [], 3, 0, 0, 4));
