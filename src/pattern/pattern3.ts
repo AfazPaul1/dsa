53b67d7a5ef9d53ac5359d68ce9ecad71bdf14e6
AfazPaul1 <afazpaul@gmail.com>
2025-09-26 18:58:57 +0530

    an approach with fewer changes compared to pattern 1

--- src/pattern/pattern3.ts
+++ src/pattern/pattern3.ts
@@ -1,11 +1,11 @@
 function pattern3(n:number) {
-    for (let i = n; i > 0; i--) {
+    for (let i = 0; i < n; i++) {
         let pattern = ""
-        for (let j = 0; j < i; j++) {
+        for (let j = 0; j < n-i; j++) {//cause we start from 0
             pattern+="*"
         }
         console.log(pattern)
     }
     return true
-}
+} 
 console.log(pattern3(5))
