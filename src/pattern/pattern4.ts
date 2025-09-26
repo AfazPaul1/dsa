855a3e2b3d3961f6d26468a90b74d626b08435e4
AfazPaul1 <afazpaul@gmail.com>
2025-09-26 20:19:30 +0530

    misc

--- src/pattern/pattern4.ts
+++ src/pattern/pattern4.ts
@@ -3,12 +3,8 @@ function pattern4(n:number) {
         let pattern = ""
         for (let j = 1; j <= i+1; j++) {
             pattern+=j
-
         }
-        console.log(pattern);
-        
-        
+        console.log(pattern);     
     }
-
 }
 console.log(pattern4(5));
