188f49c0bb8b972e472ef159ef77090c9a5cafd9
AfazPaul1 <afazpaul@gmail.com>
2025-09-28 01:30:55 +0530

    17 actually

--- src/pattern/pattern30.ts
+++ src/pattern/pattern30.ts
@@ -2,7 +2,7 @@ function pattern30(n) {
     for (let i = 1; i < 2*n; i++) {
         let pattern = ""
         let numberOfSpaces = i <= n? (2*n)-i : i
-        let noOfCols =  i > n ? (2*n)-i : i
+        let noOfCols =  i > n ? (2*n)-i : i // have already seen this before since pattern 5 where it increases then decreases
         for (let s = 0; s < numberOfSpaces; s++) {
             pattern+="  "//double space
         }
@@ -17,4 +17,17 @@ function pattern30(n) {
         
     }
 }
-console.log(pattern30(5));
+console.log(pattern30(4));
+//17
+//                   1 
+//                 2 1 2 
+//               3 2 1 2 3
+//             4 3 2 1 2 3 4
+//           5 4 3 2 1 2 3 4 5
+//             4 3 2 1 2 3 4
+//               3 2 1 2 3
+//                 2 1 2
+//                   1
+// simply no of cols is just it starts from that column no and goes till that ciolumn number
+//plainly that
+//we compute it
