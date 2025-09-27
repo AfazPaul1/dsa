f528c493cbccf05406975f405750b356acad2496
AfazPaul1 <afazpaul@gmail.com>
2025-09-27 21:09:23 +0530

    getting closer working out the spaces

--- src/pattern/pattern30.ts
+++ src/pattern/pattern30.ts
@@ -1,14 +1,15 @@
 function pattern30(n) {
-    for (let i = 1; i <= n; i++) {
+    for (let i = 1; i < 2*n; i++) {
         let pattern = ""
-        let numberOfSpaces = n-i
+        let numberOfSpaces = i > n? i-n:(2*n)-i
+        let noOfCols =  i > n ? (2*n)-i : i
         for (let s = 0; s < numberOfSpaces; s++) {
             pattern+="  "//double space
         }
-        for (let j = i; j > 0; j--) {
+        for (let j = noOfCols; j > 0; j--) {
             pattern+=`${j} `
         }
-        for (let k = 2; k <= i; k++) { // was confused how i was going to do the increasing pattern realized that i could just do another for which increases and adds to the pattern no need to look for a condition which works for both
+        for (let k = 2; k <= noOfCols    ; k++) { // was confused how i was going to do the increasing pattern realized that i could just do another for which increases and adds to the pattern no need to look for a condition which works for both
             pattern+=`${k} `
             
         }
