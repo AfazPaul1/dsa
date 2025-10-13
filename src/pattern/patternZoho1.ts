1d823921e7a6388660a0eea36f4b93cff41593c6
AfazPaul1 <afazpaul@gmail.com>
2025-10-13 12:12:33 +0530

    i got the first 4 just the reverse i need to do

--- src/pattern/patternZoho1.ts
+++ src/pattern/patternZoho1.ts
@@ -1,29 +1,38 @@
 function patternZoho(n) {
+    
+    let sum = 0
     for (let i = 1; i <=2*n; i++) {
         let noOfSpaces = i <= n ? (2*n)-i :i        
         let noOfCols = i > n ? (2*n)-i : i
         let pattern = ""
-        let tempPattern = ""
-        for (let j = 0; j <= noOfSpaces ; j++) {
+        for (let s = 0; s <= noOfSpaces ; s++) {
             pattern+=" "
         }
-        for (let k = noOfCols; k > 0; k--) {
-            tempPattern+=`${k} `
-            
+        sum+=noOfCols
+        for (let i = sum; i >= noOfCols; i--) {
+            pattern+=` ${i}`
         }
-        pattern+=reverseStr(tempPattern)
         console.log(pattern);
         
     }
 }
 console.log(patternZoho(4));
-function reverseStr(str) {
-    return (str === "") ? "" : reverseStr(str.substr(1)) + str.charAt(0)
-}
-//  1
-//         1 2
-//        1 2 3
-//       1 2 3 4
-//        1 2 3
-//         1 2
 //          1
+//         3 2
+//        6 5 4 3
+//       10 9 8 7 6 5 4
+//        13 12 11 10 9 8 7 6 5 4 3
+//         15 14 13 12 11 10 9 8 7 6 5 4 3 2
+//          16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1
+//           16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 0      
+// undefined
+//expected
+//i should find out what to print i can see that the nuber to print is the sum of previous numbers
+//     1
+//    3 2
+//   6 5 4
+// 10 9 8 7
+// 10 9 8 7
+//   6 5 4
+//    3 2
+//     1
