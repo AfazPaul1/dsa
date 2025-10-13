cfa0f0ec0f2e73b88beef07a9164f542de8069ed
AfazPaul1 <afazpaul@gmail.com>
2025-10-13 16:35:42 +0530

    answer

--- src/zoho/4.ts
+++ src/zoho/4.ts
@@ -1,11 +1,24 @@
 function arrP(arr) {
+    let hash = new Map()
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === arr[i+1]) {
-            //this wont work cause it'll be true for i = 1 and 2 causing 2 4's in out which we do not want
+        hash.set(arr[i], (hash.get(arr[i]) || 0) + 1)
+        if (hash.get(arr[i]) === 2 && arr[i] !== 0) {
+            arr[i-1] = i*2
+            hash.set(arr[i], 0)
+            arr[i] = 0
+            hash.set(0, (hash.get(0) || 0) + 1) //cause we are setting already explored sections to 0
+            //console.log(i*2); only prints multiples not existing numbers
+        }
+    }
+    for (let k = 0; k < arr.length; k++) {
+        if (arr[k] !== 0) {
+            console.log(arr[k]);
             
         }
-        
     }
+    for (let j = 1; j <= hash.get(0); j++) {
+        console.log(0);  
+    }   
 }
 const arr = [0,2,2,2,0,6,6,0,8]
 console.log(arrP(arr))
