25dd9b45ec76917652f3b6f0fee75baf90dbad05
AfazPaul1 <afazpaul@gmail.com>
2025-10-13 18:17:21 +0530

    no hash custom deduplication

--- src/zoho/6.ts
+++ src/zoho/6.ts
@@ -1,18 +1,34 @@
 function mergeArr(arr1, arr2) {
-    const hash = new Set()
+    const res = []
     let i = 0
     let j = 0
-    while(j < 6) {
+    while(i < arr1.length && j < arr2.length) {
+        let eleToAdd = 0
         if (arr1[i] <= arr2[j]) {
-            hash.add(arr1[i])
+            eleToAdd = arr1[i]
             i++
         } else{
-            hash.add(arr2[j])
+            eleToAdd = arr2[j]
             j++
         }
+        if (res.length === 0 || eleToAdd !== res[res.length -1 ]  ) {
+            res.push(eleToAdd)
+        }
+    }
+    while(i < arr1.length) {
+        if (arr1[i] !== res[res.length -1 ]) {
+            res.push(arr1[i])    
+        }
+        i++
+    }
+    while(j < arr2.length) {
+        if (arr2[j] !== res[res.length-1]) {
+            res.push(arr2[j])    
+        }
+        j++
     }
     
-    console.log(hash);
+    console.log(res);
     
 }
 console.log(mergeArr([1,2,3,4,10], [1, 9,10,11]));
