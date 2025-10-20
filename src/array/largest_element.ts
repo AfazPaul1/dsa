8c5850aaa53bb3327792a9607999d60fa10ab4af
AfazPaul1 <afazpaul@gmail.com>
2025-10-20 22:28:41 +0530

    execution time was 120ms with the insertions sort on the same array.

--- src/array/largest_element.ts
+++ src/array/largest_element.ts
@@ -12,6 +12,7 @@ console.time("minSubsets execution time");
 console.log(largest_element(largeArray));
 console.timeEnd("minSubsets execution time");
 //minSubsets execution time: 5.168ms
+//execution time was 120ms with the insertions sort on the same array
 //console.log(largest_element([3,2,1,5,2]));
 //console.log(largest_element([14, 9, 15, 12, 6, 8, 13]));
 
