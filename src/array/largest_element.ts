cd3023a5d80567b39a53c7cdee8edb770dc5268d
AfazPaul1 <afazpaul@gmail.com>
2025-10-20 22:12:29 +0530

    - Optimal solution. tc: O(n) cause only one loop through the length of the array.

--- src/array/largest_element.ts
+++ src/array/largest_element.ts
@@ -1,18 +1,10 @@
 function largest_element(arr){
-    arr = insertionSort(arr, arr.length)
-    return arr[arr.length-1]
+    let max = arr[0]
+    for (let i = 1; i < arr.length; i++) {
+        if(arr[i] > max) max =  arr[i]
+    }
+    return max
 }
 console.log(largest_element([3,2,1,5,2]));
 console.log(largest_element([14, 9, 15, 12, 6, 8, 13]));
-function insertionSort(arr, n) {
-    for(let i = 0; i < n; i++) {
-        let j = i
-        while( j > 0 && arr[j-1] > arr[j]) {
-            [arr[j-1], arr[j]] = [arr[j], arr[j-1]]
-            j--
-        }
-    }
-    return arr
-}
-//console.log(insertionSort([14, 9, 15, 12, 6, 8, 13], 7));
-//ok    
+  
