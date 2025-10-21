b0793ea36508c868ec00936ada943ea407051dd9
AfazPaul1 <afazpaul@gmail.com>
2025-10-21 20:10:23 +0530

    we use the optimal approach to find max and use another loop to go through the entire array again and find the secondlargest by comapring each value with the max value. since we traverse the array twice the tc is 2N

--- src/array/second_largest.ts
+++ src/array/second_largest.ts
@@ -1,22 +1,17 @@
 function second_largest(arr) {
-    arr.sort((a, b) => a-b) //nlogn
-    let i = arr.length-2
+    let max  = arr[0]
     let secondLargest = -1
-    while(arr[i] === arr[arr.length-1] && i >= 0) {
-        i--
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) max=arr[i]
     }
-    if(i < 0) {
-        console.log(" no second largest");
-        return
-    } else {
-        secondLargest =  arr[i]
-        return secondLargest
+    for (let j = 0; j < arr.length; j++) {
+        if(arr[j] !== max && arr[j] > secondLargest) secondLargest = arr[j]
     }
-    
+    return secondLargest
 }
 
-console.log(second_largest([14, 9, 15, 12, 6, 8, 13, 15,15,15]));
+console.log(second_largest([14, 9, 15, 12, 6, 8, 13, 16]));
 console.log(second_largest([3,2,1,5,2, 5]));
-console.log(second_largest([7,7,7,7,7]));
+//console.log(second_largest([7,7,7,7,7]));
 
 
