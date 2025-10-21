fbdb09cc8389062f9b66f9398d004f2948ea764c
AfazPaul1 <afazpaul@gmail.com>
2025-10-21 20:22:11 +0530

    optimal O(n) approach based on the principle that if some value becomes the largest then the previous largest value becomes the second largest. but this approach wont work if we encounter the max value before we meet the second largest. in the present form it skips if the value is lesser than max and we dont check check whether its greater than secondLargest

--- src/array/second_largest.ts
+++ src/array/second_largest.ts
@@ -1,16 +1,18 @@
 function second_largest(arr) {
     let max  = arr[0]
-    let secondLargest = -1
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] > max) max=arr[i]
-    }
-    for (let j = 0; j < arr.length; j++) {
-        if(arr[j] !== max && arr[j] > secondLargest) secondLargest = arr[j]
+    let secondLargest = 0
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i] > max) {
+            secondLargest = max
+            max = arr[i]
+        }
     }
     return secondLargest
 }
 
-console.log(second_largest([14, 9, 15, 12, 6, 8, 13, 16]));
+console.log(second_largest([14, 9, 12, 6, 8, 13, 16, 15]));
+//14
+//3
 console.log(second_largest([3,2,1,5,2, 5]));
 //console.log(second_largest([7,7,7,7,7]));
 
