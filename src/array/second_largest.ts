dd6304ce4a02bad6fd0d8b80657a7e4f6069f3d2
AfazPaul1 <afazpaul@gmail.com>
2025-10-21 20:52:02 +0530

    corrected optimal O(n) approach. this approach keeps track of the second largest at every point not only in cases where we encounter the largest.

--- src/array/second_largest.ts
+++ src/array/second_largest.ts
@@ -1,17 +1,20 @@
 function second_largest(arr) {
     let max  = arr[0]
-    let secondLargest = 0
+    let secondLargest = -1
     for (let i = 0; i < arr.length; i++) {
         if(arr[i] > max) {
             secondLargest = max
             max = arr[i]
         }
+        else if(arr[i] > secondLargest && arr[i] < max) {
+            secondLargest = arr[i]
+        }//else if is executed the most times so should i swap the conditions?
     }
     return secondLargest
 }
 
-console.log(second_largest([14, 9, 12, 6, 8, 13, 16, 15]));
-//14
+console.log(second_largest([14, 9, 12, 6, 8, 13, 18, 15, 8, 9, 2, 5, 16]));
+//16
 //3
 console.log(second_largest([3,2,1,5,2, 5]));
 //console.log(second_largest([7,7,7,7,7]));
