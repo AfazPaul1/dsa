69e9ef7374be052ec807cd3f6d323ab90205d6a9
AfazPaul1 <afazpaul@gmail.com>
2025-10-21 18:54:47 +0530

    simple loop

--- src/array/second_largest.ts
+++ src/array/second_largest.ts
@@ -1,8 +1,12 @@
 function second_largest(arr) {
     arr.sort((a, b) => a-b)
-    return arr[arr.length-2]
+    let i = arr.length-2
+    while(arr[i] === arr[arr.length-1]) {
+        i--
+    }
+    return arr[i]
 }
 
-console.log(second_largest([14, 9, 15, 12, 6, 8, 13, 15]));
+console.log(second_largest([14, 9, 15, 12, 6, 8, 13, 15,15,15]));
 console.log(second_largest([3,2,1,5,2, 5]));
 
