f1a68de0299875da6ae02e4d45d804b8bd357504
AfazPaul1 <afazpaul@gmail.com>
2025-10-21 19:40:37 +0530

    adjusted for case where all elements in the array are the same. nlogn + N solution cause of sorting and worst case we'll have to traverse the entire array to find the second largest element

--- src/array/second_largest.ts
+++ src/array/second_largest.ts
@@ -1,12 +1,22 @@
 function second_largest(arr) {
-    arr.sort((a, b) => a-b)
+    arr.sort((a, b) => a-b) //nlogn
     let i = arr.length-2
-    while(arr[i] === arr[arr.length-1]) {
+    let secondLargest = -1
+    while(arr[i] === arr[arr.length-1] && i >= 0) {
         i--
     }
-    return arr[i]
+    if(i < 0) {
+        console.log(" no second largest");
+        return
+    } else {
+        secondLargest =  arr[i]
+        return secondLargest
+    }
+    
 }
 
 console.log(second_largest([14, 9, 15, 12, 6, 8, 13, 15,15,15]));
 console.log(second_largest([3,2,1,5,2, 5]));
+console.log(second_largest([7,7,7,7,7]));
+
 
