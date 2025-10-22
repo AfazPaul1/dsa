5ceb6f68e3bbc1754450c801a2d5cda22e35e6c0
AfazPaul1 <afazpaul@gmail.com>
2025-10-22 19:12:11 +0530

    optimal actual 2 pointer pattern. The fast pointer advances every iteration. The slow pointer only advances when a certain condition is met (eg a unique element is found or the "window" needs to shrink). tc: O(N).

--- src/array/remove_duplicates.ts
+++ src/array/remove_duplicates.ts
@@ -4,23 +4,13 @@
 //Explanation: Total number of unique elements are 3, i.e[1,2,3] and Therefore return 3 after assigning [1,2,3] in the beginning of the array.
 function removeDuplicates(arr) { 
     let i = 0
-    let j = 0
-    while( i < arr.length ){
-        if(arr[i] !== arr[i+1]) {
+    for (let j = 1; j < arr.length; j++) {
+        if(arr[i] !== arr[j]) {
+            arr[i+1] = arr[j]
             i++
-            continue
         }
-        j = i
-        while(arr[j] === arr[j+1] && j <= arr.length-1) {
-            j++
-        }
-        if(j >= arr.length-1) return i+1
-        arr[i+1] = arr[j+1]
-        i++
     }
-    return arr
-    
-
+    return i+1
 }
 console.log(removeDuplicates([1,2,3,3,3,3,4,4,4,5]));
 //[ 1,2,3,4,3,4,5,4,5,5,]
