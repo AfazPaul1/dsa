ce1eb95cdea25b11ef1eafb82710555efeb1bd21
AfazPaul1 <afazpaul@gmail.com>
2025-10-23 14:31:41 +0530

    optimal approach is O(1) extra space complexity instead of creating O(d) temp array. is to reverse 0,d and d,n separately and 0,n as whole. this results in the same. but tc increases. - 1st reverse: O(d) - 2nd: (n-d) - 3rd: n hence 2N tc. time complexity increases but sc is O(1)

--- src/array/rotate_by_d.ts
+++ src/array/rotate_by_d.ts
@@ -1,15 +1,19 @@
 //either left or right
 function rotate_by_d(arr, d) {
-    let temp = arr.slice(0, d)
     let n = arr.length
-    d = d%n
-    for (let i = d; i < n; i++) {
-        arr[i-d] = arr[i]
-    }
-    for (let j = n-d; j < n; j++) {
-        arr[j] = temp[j-(n-d)]
-    }
+    reverse(arr, 0, d-1)
+    reverse(arr, d, n-1)
+    reverse(arr, 0, n-1)
     return arr
-}
-console.log(rotate_by_d([1, 2, 2, 7, 3, 4, 5, 6, 6], 3));
+ }
+ console.log(rotate_by_d([1, 2, 2, 7, 3, 4, 5, 6, 6], 3));
 //[7, 3, 4, 5, 6, 6, 1, 2, 2]
+function reverse(arr, startIndex, endIndex) {
+    if (startIndex >= endIndex) {
+        return arr
+    }
+    [arr[startIndex], arr[endIndex]] = [arr[endIndex], arr[startIndex]]
+    //return reverse(arr, startIndex++, endIndex--) // When the JavaScript engine evaluates startIndex++ as a function argument, it uses the original value of startIndex for the function call first. will result in stack overflow cause the value never changed stays 2,0 and it continues on and on
+    return reverse(arr, ++startIndex, --endIndex)
+}
+//console.log(reverse([1,2,3], 0, 2));
