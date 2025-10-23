290c69bbda4af385c8f71ad7cd9233807e241a46
AfazPaul1 <afazpaul@gmail.com>
2025-10-23 11:23:03 +0530

    could also do it this way. - tc: - O(d) for the temp creation - O(n-d) for shifting - O(d) again for insertion - hence O(n+d) - extra space used - O(d) temp array - optimal solution could be without using the extra space

--- src/array/rotate_by_d.ts
+++ src/array/rotate_by_d.ts
@@ -6,9 +6,10 @@ function rotate_by_d(arr, d) {
     for (let i = d; i < n; i++) {
         arr[i-d] = arr[i]
     }
-    for (let j = 0; j < d; j++) {
-        arr[(n-d)+j] = temp[j]
+    for (let j = n-d; j < n; j++) {
+        arr[j] = temp[j-(n-d)]
     }
     return arr
 }
 console.log(rotate_by_d([1, 2, 2, 7, 3, 4, 5, 6, 6], 3));
+//[7, 3, 4, 5, 6, 6, 1, 2, 2]
