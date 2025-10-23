41cca68fdcd8b3fc0a412b13a069a854d3e703dd
AfazPaul1 <afazpaul@gmail.com>
2025-10-23 16:47:57 +0530

    misc

--- src/array/move_zeroes_end.ts
+++ src/array/move_zeroes_end.ts
@@ -1,16 +1,12 @@
 function move_zeroes_to_end(arr:number[]) {
-    let temp = []
+    let j = -1 
+    //using a loop to find the first instance where 0 occurs
+    //that was what was confusing me
     for (let i = 0; i < arr.length; i++) {
-        if(arr[i] !== 0) {
-            temp.push(arr[i])
+        if (arr[i]!==0) {
+            [arr[j], arr[i]]=[arr[i], arr[j]]
+            j=i
         }
     }
-    for (let j = 0; j < arr.length; j++) {
-        arr[j] = temp[j]
-    }
-    for (let k = temp.length; k < arr.length; k++) {
-        arr[k] = 0
-    }
-    return arr
 }
 console.log(move_zeroes_to_end([7, 0, 4, 5, 0, 6, 1, 2, 2]));
