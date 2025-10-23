6c0ceec5f5c43fe6803c3d523a717cb47a387ab9
AfazPaul1 <afazpaul@gmail.com>
2025-10-23 18:39:48 +0530

    if the rotation is the size of n there will be no changes accomodating for that.

--- src/array/rotate_by_d.ts
+++ src/array/rotate_by_d.ts
@@ -1,12 +1,14 @@
 //either left or right
 function rotate_by_d(arr, d) {
     let n = arr.length
+    d=d%n
     reverse(arr, 0, d-1)
     reverse(arr, d, n-1)
     reverse(arr, 0, n-1)
     return arr
  }
  console.log(rotate_by_d([1, 2, 2, 7, 3, 4, 5, 6, 6], 3));
+ console.log(rotate_by_d([1, 2, 2, 7, 3, 4, 5, 6, 6], 30));
 //[7, 3, 4, 5, 6, 6, 1, 2, 2]
 function reverse(arr, startIndex, endIndex) {
     if (startIndex >= endIndex) {
