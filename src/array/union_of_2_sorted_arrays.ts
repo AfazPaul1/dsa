cd98739a2a2c72e343f3a450bc1c6f6f012f51d2
AfazPaul1 <afazpaul@gmail.com>
2025-10-24 11:11:32 +0530

    brute approach. since sets are ordered based on insertion and are not sorted unlike c++ sets. we need to sort after insertion. - tc: O(n) & O(m) for  set creation. O(n+m) or O(k) for union creation. klogk for sorting.  the sorting is the bottleneck making the final result dominated by sorting time. O(n+m)log(n+m) - sc: O(n+m) for the union and O(n+m) for the set - thisworks irrespective of n amd m are sorted. so an optimal approach would use the fact that both are sorted

--- src/array/union_of_2_sorted_arrays.ts
+++ src/array/union_of_2_sorted_arrays.ts
@@ -1,12 +1,16 @@
 function union_of_2_sorted_arrays(arr1, arr2, n, m) {
-    const res = new Set() 
+    const res = new Set<number>() 
     for (let i = 0; i < arr1.length; i++) {
         res.add(arr1[i])
     }
     for (let j = 0; j < arr2.length; j++) {
         res.add(arr2[j])   
     }
-    return res
+    let union = []
+    for (const ele of res) {
+        union.push(ele)
+    }
+    return union.sort((a,b) => a-b)
 }
 console.log(union_of_2_sorted_arrays([1,2,3,4,6,6,6,7,8,9], [2,3,4,4,5,24], 5, 5));
  //Set(10) { 1, 2, 3, 4, 6, 7, 8, 9, 5, 24 }    
