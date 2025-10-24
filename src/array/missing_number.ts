44d9db3a27d177717a5f5a6c3c33376daffc6e97
AfazPaul1 <afazpaul@gmail.com>
2025-10-25 00:01:26 +0530

    - missing number - optimal - xor method if we xor all these all that'll be left is the number without the pair ie the number we want - but this is 2N - there is a better way

--- src/array/missing_number.ts
+++ src/array/missing_number.ts
@@ -1,10 +1,13 @@
 //summation
 function missing_number(arr, N) {  
-    let sum = 0
+    let xor1 = 0
+    let xor2 = 0
     for (let i = 0; i < arr.length; i++) {
-        sum+=arr[i]
+        xor1 = xor1^arr[i]
     }
-    let ogSum = N*(N+1)/2
-    return ogSum-sum
+    for (let j = 1; j <= N; j++) {
+        xor2 = xor2^j
+    }
+    return xor1^xor2
 }    
 console.log(missing_number([1,2,4,5], 5));
