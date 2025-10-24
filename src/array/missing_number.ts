01cda24708c4dfdd6448b89021395ae8f5515173
AfazPaul1 <afazpaul@gmail.com>
2025-10-25 00:11:02 +0530

    - missing number - incorrect approach the arr[j-1] goes from 0 to 4.  we should loop using the smaller array which is n-1 and add the extra value which is just one value outside the loop

--- src/array/missing_number.ts
+++ src/array/missing_number.ts
@@ -2,12 +2,11 @@
 function missing_number(arr, N) {  
     let xor1 = 0
     let xor2 = 0
-    for (let i = 0; i < arr.length; i++) {
-        xor1 = xor1^arr[i]
-    }
     for (let j = 1; j <= N; j++) {
+        xor1=xor1^arr[j-1]
         xor2 = xor2^j
     }
+    xor1=xor1^arr[arr.length-1]
     return xor1^xor2
 }    
-console.log(missing_number([1,2,4,5], 5));
+console.log(missing_number([1,2,4,5], 5)); //6
