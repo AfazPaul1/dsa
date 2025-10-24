f55c66f99fd8c70ced2c78c3c0fbac5dc73ebacf
AfazPaul1 <afazpaul@gmail.com>
2025-10-25 00:18:13 +0530

    - missing numner - optimal approach - tc: O(n) only single loop - sc: O(1) constant

--- src/array/missing_number.ts
+++ src/array/missing_number.ts
@@ -1,12 +1,11 @@
-//summation
 function missing_number(arr, N) {  
     let xor1 = 0
     let xor2 = 0
-    for (let j = 1; j <= N; j++) {
-        xor1=xor1^arr[j-1]
-        xor2 = xor2^j
+    for (let i = 0; i < arr.length; i++) {
+        xor1 = xor1^arr[i]
+        xor2=xor2^(i+1)
     }
-    xor1=xor1^arr[arr.length-1]
+    xor2=xor2^N
     return xor1^xor2
 }    
-console.log(missing_number([1,2,4,5], 5)); //6
+console.log(missing_number([1,2,4,5], 5));
