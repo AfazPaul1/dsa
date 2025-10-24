c11914c3d758ac109dd7a209f1c54425e37ab986
AfazPaul1 <afazpaul@gmail.com>
2025-10-24 15:16:29 +0530

    - this works only in some cases - there were three cases to be handled which i didnt identify. since i kept incrementing irrespective of whether it matched or not and only j incremented when it matched this meant that suppose at i is a number which didnt match with where j is now because j is at a smaller number and eventually that i will indeed come in j but irrespectively i increments and is lost. the greater than lesser than logic should be handled

--- src/array/intersection_sorted_arrays.ts
+++ src/array/intersection_sorted_arrays.ts
@@ -1,18 +1,20 @@
 //WE ARE SEEING INTERSECTION SO IF 4 OCCURS 2 TIMES IN BOTH THEN IN THE OUTPUT 4 WILL BETHERE 2 TIMES
 function intersection_sorted_arrays(a, b) {
     const visited = Array(b.length).fill(-1)
-    const intersection = []
-    for (let i = 0; i < a.length; i++) {
-        for (let j = 0; j < b.length; j++) {
-            if(a[i] === b[j] && visited[j] === -1) {
-                intersection.push(a[i])
-                visited[j]=1
-                break
-            }
-            if(b[j] > a[i]) break
+    const intersection:number[] = []
+    let i = 0
+    let j = 0
+    let n = a.length
+    let m = b.length
+    while(i<n && j<m) {
+        if(a[i] === b[j]) {
+            intersection.push(b[j])
+            j++
         }
+        i++
     }
     return intersection
 }
-console.log(intersection_sorted_arrays([1,2,3,4,4,6,6,6,7,8,9], [2,3,4,4,5,24]));
+console.log(intersection_sorted_arrays([1,2,3,4,4,6,6,6,7,8,9], 
+    [2,3,4,4,5,24]));
 //[ 2, 3, 4, 4 ]
