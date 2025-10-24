f6f94e2a8c820552f5d8a7f89549c43972aae1eb
AfazPaul1 <afazpaul@gmail.com>
2025-10-24 15:28:20 +0530

    - tc: O(n+m) worst case best case only n or m. worst case is when if and else if alternate untill end. best case is if only if or else keeps executng till exhaustion - sc: O(1)

--- src/array/intersection_sorted_arrays.ts
+++ src/array/intersection_sorted_arrays.ts
@@ -7,14 +7,20 @@ function intersection_sorted_arrays(a, b) {
     let n = a.length
     let m = b.length
     while(i<n && j<m) {
-        if(a[i] === b[j]) {
+        if(a[i] < b[j]) i++
+        else if(b[j] < a[i]) j++
+        else {
             intersection.push(b[j])
             j++
+            i++
         }
-        i++
     }
     return intersection
 }
-console.log(intersection_sorted_arrays([1,2,3,4,4,6,6,6,7,8,9], 
-    [2,3,4,4,5,24]));
+console.log(intersection_sorted_arrays([1,2,3,4,4,6,6,6,7,8,9], [2,3,4,4,5,24]));
 //[ 2, 3, 4, 4 ]
+console.log(intersection_sorted_arrays([1,2,6,6,6,7,8,9], 
+    [2,3,4,4,5,6,9]));
+//[ 2, 6, 9 ]
+ console.log(intersection_sorted_arrays([1,2,3,6,6,6,7,8,9], 
+    [2,9]));
