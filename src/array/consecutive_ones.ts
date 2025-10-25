472860f9b4df19e27d3a47003a0fb87de4cc0e0a
AfazPaul1 <afazpaul@gmail.com>
2025-10-25 23:14:20 +0530

    - consecutive_ones - optimal approach - tc: O(n) - sc: O(1) maybe i can improve it

---
+++ src/array/consecutive_ones.ts
@@ -0,0 +1,16 @@
+function consecutive_ones(arr) {
+    let max = 0
+    let conseq = 0
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 1) {
+            conseq +=1
+        } else {
+            conseq=0
+        }
+        max = Math.max(max, conseq)
+    }
+    
+    return max
+}
+console.log(consecutive_ones([1,1,0,1,1,1]));
+
