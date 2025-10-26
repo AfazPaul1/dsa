14899bd8484e274fd44572338c2fc4019e5ca19d
AfazPaul1 <afazpaul@gmail.com>
2025-10-26 21:19:47 +0530

    - get_single_element - optimal xor method - tc: O(n) - sc: O(1) - the element witout a pair pops out

--- src/array/get_single_element.ts
+++ src/array/get_single_element.ts
@@ -1,10 +1,9 @@
 function get_single_element(arr) {
-    const hash = new Map<Number, number>()
-    for (const element of arr) {
-        hash.set(element, (hash.get(element) || 0) + 1)
+    let xor1 = 0
+    for (let i = 0; i < arr.length; i++) {
+        xor1 = xor1 ^ arr[i]
     }
-    for (const [element, value] of hash) {
-        if(value === 1) return element
-    }
+    return xor1
 }
-console.log(get_single_element([1,7, 2,3, 1,2, 4, 3,7 ]));
+console.log(get_single_element([1, 7, 2, 3, 1, 2, 4, 3, 7 ]));
+//4
