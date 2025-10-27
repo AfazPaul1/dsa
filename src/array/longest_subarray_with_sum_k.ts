6a0b143abc13a1c9e72384aca412242a2c6b5683
AfazPaul1 <afazpaul@gmail.com>
2025-10-28 00:50:01 +0530

    - longest_subarray_with_sum_k - optimal for only positives - incomplete - it doesntwork when only the first element matches

--- src/array/longest_subarray_with_sum_k.ts
+++ src/array/longest_subarray_with_sum_k.ts
@@ -1,21 +1,22 @@
 function longest_subarray_with_sum_k(arr, k) {
-
-    let hash = new Map<number, number>()
-    let sum = 0
-    let length = 0
-    for (let i = 0; i < arr.length; i++) {
-        sum+=arr[i]
-        if(!hash.has(sum)) hash.set(sum, i)
-        if(sum===k) {
-            //console.log(i);
-            length = Math.max(length, i+1)
+    let left = 0
+    let right = 0
+    let sum = arr[0]
+    let n = arr.length
+    let maxLen = 0
+    while( right < n-1 ) {
+        right++;
+        sum+=arr[right]
+        while(sum>k) {
+            sum-=arr[left]
+            left++
         }
-        if (hash.has(sum-k)) {
-            length = Math.max(length, i - hash.get(sum-k)! )
-            //console.log(sum, i,length);
+        if(sum===k) {
+            maxLen=Math.max(maxLen, (right+1)-left)
         }
     }
-    return [length]
+
+    return  maxLen
 }
 //console.log(longest_subarray_with_sum_k([2,3,5,1,9], 10));//3
 //console.log(longest_subarray_with_sum_k([2,3,5], 5));//2
@@ -23,24 +24,7 @@ function longest_subarray_with_sum_k(arr, k) {
 //console.log(longest_subarray_with_sum_k([1,2,3,1,1,1,1,4,2,3], 3));
 //console.log(longest_subarray_with_sum_k([2,0,0,3], 3));//3
 //console.log(longest_subarray_with_sum_k([2], 2));
-console.log(longest_subarray_with_sum_k([1,2,2 ,-1,1,1,1,4,2,3], 4));
-//when i is at 6, sum is 7, 7-k(4) is 3, its value(index) is 1 in hash, 6-1 is 5 which is [2,-1,1,1,1] which sums to 4 which is the largest subarray
-// 5 2 2
-// 3
-// 5 4 4
-// 7 6 5
-// 11 7 5
-// [
-//   5,
-//   Map(9) {
-//     1 => 0,
-//     3 => 1,
-//     5 => 2,
-//     4 => 3,
-//     6 => 5,
-//     7 => 6,
-//     11 => 7,
-//     13 => 8,
-//     16 => 9
-//   }
-// ]
+//console.log(longest_subarray_with_sum_k([1,2,2,-1,1,1,1,4,2,3], 4));
+//console.log(longest_subarray_with_sum_k([1,2,3,1,1,1,1,3,3], 6));//4
+console.log(longest_subarray_with_sum_k([1,2,3,1,1,1,1,3,19], 19));//1 changed while condition to n-1 to make sure loop doesnt run for right = 9
+console.log(longest_subarray_with_sum_k([19,2,3,1,1,1,1,3,1], 19));//0 incorrect //this is due to use never checking for 1st element we immediately sum it with next as it eneters the loop
