96912bdb74a59463129eded04a8328b4c899e4f1
AfazPaul1 <afazpaul@gmail.com>
2025-10-27 15:01:48 +0530

    - longest_subarray_with_sum_k - still brute but this is n*n - we have removed the reduce method call and in every iteration w ehave accumulated the value and checked it against the sum - tc: n*n

--- src/array/longest_subarray_with_sum_k.ts
+++ src/array/longest_subarray_with_sum_k.ts
@@ -1,17 +1,17 @@
 function longest_subarray_with_sum_k(arr, k) {
-    
+
     let longest_length = 0
+    let sum = 0
     for (let i = 0; i < arr.length; i++) {
-        
+        sum = 0
         for (let j = i; j < arr.length; j++) {
             let newArr = arr.slice(i,j+1)
-            let sum=newArr.reduce((acc, cur) => acc+cur)
+            sum+=arr[j]
             let count=newArr.length
             if(sum===k) {
                 console.log(newArr);
                 longest_length=Math.max(count, longest_length)
-            }
-              
+            }  
         }
     }
     return longest_length
