9da4dfb06d0153f9d89a6bce14ad2b11d39cca7c
AfazPaul1 <afazpaul@gmail.com>
2025-10-27 20:08:37 +0530

    - longest_subarray_with_sum_k - better solution using hashing - we keep building a hash of size input array which stores the sum upto that point. like if i = 3 then we create a hash sum, index which is the prefixsum at that index - works on the basis that at some point if theres a sum eg: 9 and the number to find is k (3) then if theres a prefix sum of 6 (sum-k) in the hash, then from that prefix index to the current index must sum to k (3) we have found our subarray now we just check max length - also need to handle cases where the sum upto that point itself is k. happens only once early or another time if theres a 0 next to it (apparently this wont work. refer notes) - tc: O(n) only 1 loop and hashing operations are mostly O(1) constant time - sc: O(n) for the hashmap

--- src/array/longest_subarray_with_sum_k.ts
+++ src/array/longest_subarray_with_sum_k.ts
@@ -3,12 +3,15 @@ function longest_subarray_with_sum_k(arr, k) {
     let hash = new Map<number, number>()
     let sum = 0
     let length = 0
-    let total = arr.reduce((acc, cur) => cur+acc)
     for (let i = 0; i < arr.length; i++) {
         sum+=arr[i]
         hash.set(sum, i)
-        if (hash.has(total-k)) {
-            length = Math.max(length, arr.length-(i+1))
+        if(sum===k) {
+            
+            length = Math.max(length, i+1)
+        }
+        if (hash.has(sum-k)) {
+            length = Math.max(length, i- hash.get(sum-k)! )//SINCE I HAVE ALREADY checked that it exists using has i am using rhe non null assertion
         }
     }
     
@@ -17,3 +20,4 @@ function longest_subarray_with_sum_k(arr, k) {
 //console.log(longest_subarray_with_sum_k([2,3,5,1,9], 10));//3
 console.log(longest_subarray_with_sum_k([2,3,5], 5));//2
 console.log(longest_subarray_with_sum_k([2,3,4,1,9], 10));//3
+console.log(longest_subarray_with_sum_k([1,2,3,1,1,1,1,4,2,3], 3));
