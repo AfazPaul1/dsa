bfa3ff59237b754dc68348422b97f3da3d384308
AfazPaul1 <afazpaul@gmail.com>
2025-10-27 22:36:01 +0530

    - longest_subarray_with_sum_k - better solution for array with only positives and 0 - optimal solution for array with both positives and negatives - tc: with ordered map it'll be O(nlogn) - with unordered map it'll  be O(n*1) but in the worst case the 1 can be O(n*n) happens when there is a collision every time hence n (apparently this can happen when someone figures out the collision set and ddoses us. notes-hash-perfectcollision

--- src/array/longest_subarray_with_sum_k.ts
+++ src/array/longest_subarray_with_sum_k.ts
@@ -5,19 +5,42 @@ function longest_subarray_with_sum_k(arr, k) {
     let length = 0
     for (let i = 0; i < arr.length; i++) {
         sum+=arr[i]
-        hash.set(sum, i)
+        if(!hash.has(sum)) hash.set(sum, i)
         if(sum===k) {
-            
+            //console.log(i);
             length = Math.max(length, i+1)
         }
         if (hash.has(sum-k)) {
-            length = Math.max(length, i- hash.get(sum-k)! )//SINCE I HAVE ALREADY checked that it exists using has i am using rhe non null assertion
+            length = Math.max(length, i - hash.get(sum-k)! )
+            //console.log(sum, i,length);
         }
     }
-    
-    return length
+    return [length]
 }
 //console.log(longest_subarray_with_sum_k([2,3,5,1,9], 10));//3
-console.log(longest_subarray_with_sum_k([2,3,5], 5));//2
-console.log(longest_subarray_with_sum_k([2,3,4,1,9], 10));//3
-console.log(longest_subarray_with_sum_k([1,2,3,1,1,1,1,4,2,3], 3));
+//console.log(longest_subarray_with_sum_k([2,3,5], 5));//2
+//console.log(longest_subarray_with_sum_k([2,3,4,1,9], 10));//3
+//console.log(longest_subarray_with_sum_k([1,2,3,1,1,1,1,4,2,3], 3));
+//console.log(longest_subarray_with_sum_k([2,0,0,3], 3));//3
+//console.log(longest_subarray_with_sum_k([2], 2));
+console.log(longest_subarray_with_sum_k([1,2,2 ,-1,1,1,1,4,2,3], 4));
+//when i is at 6, sum is 7, 7-k(4) is 3, its value(index) is 1 in hash, 6-1 is 5 which is [2,-1,1,1,1] which sums to 4 which is the largest subarray
+// 5 2 2
+// 3
+// 5 4 4
+// 7 6 5
+// 11 7 5
+// [
+//   5,
+//   Map(9) {
+//     1 => 0,
+//     3 => 1,
+//     5 => 2,
+//     4 => 3,
+//     6 => 5,
+//     7 => 6,
+//     11 => 7,
+//     13 => 8,
+//     16 => 9
+//   }
+// ]
