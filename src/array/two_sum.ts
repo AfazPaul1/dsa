300934e3db7b17d3b9a0377ae4e5f4593b4d36be
AfazPaul1 <afazpaul@gmail.com>
2025-10-28 12:07:34 +0530

    - two_sum - optimized brute - at i = 0 and j = 1 we have checked 2+6 != 14 - now again when i = 1 and j = 0 we again check 6+2 which is 8 - one way to not so this is have j from value next to i

--- src/array/two_sum.ts
+++ src/array/two_sum.ts
@@ -1,6 +1,6 @@
 function two_sum(arr, target) {
     for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr.length; j++) {
+        for (let j = i+1; j < arr.length; j++) {
             if(i ===j) continue
             if(arr[j] + arr[i] === target)  return [i,j]
         }
