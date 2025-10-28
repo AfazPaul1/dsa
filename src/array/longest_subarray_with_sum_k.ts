813746396590c0dfe6374c848d61cf941dee5e11
AfazPaul1 <afazpaul@gmail.com>
2025-10-28 11:45:10 +0530

    - longest_subarray_with_sum_k - tc: O(2n) - 2 while loops but the second while doesnt run n times for each instance for outer N but only N throughout all instances of N in outer loop. hence n=n = 2n at the worst case ie both left and right will be n

--- src/array/longest_subarray_with_sum_k.ts
+++ src/array/longest_subarray_with_sum_k.ts
@@ -4,9 +4,7 @@ function longest_subarray_with_sum_k(arr, k) {
     let sum = arr[0]
     let n = arr.length
     let maxLen = 0
-    while( right < n-1 ) {
-        right++;
-        sum+=arr[right]
+    while( right < n ) {
         while(sum>k) {
             sum-=arr[left]
             left++
@@ -14,17 +12,19 @@ function longest_subarray_with_sum_k(arr, k) {
         if(sum===k) {
             maxLen=Math.max(maxLen, (right+1)-left)
         }
+        right++; //right will become 9 but theres no arr[9]
+        if (right < n)  sum+=arr[right]
     }
 
     return  maxLen
 }
-//console.log(longest_subarray_with_sum_k([2,3,5,1,9], 10));//3
+console.log(longest_subarray_with_sum_k([2,3,5,1,9], 10));//3
 //console.log(longest_subarray_with_sum_k([2,3,5], 5));//2
 //console.log(longest_subarray_with_sum_k([2,3,4,1,9], 10));//3
 //console.log(longest_subarray_with_sum_k([1,2,3,1,1,1,1,4,2,3], 3));
 //console.log(longest_subarray_with_sum_k([2,0,0,3], 3));//3
 //console.log(longest_subarray_with_sum_k([2], 2));
 //console.log(longest_subarray_with_sum_k([1,2,2,-1,1,1,1,4,2,3], 4));
-//console.log(longest_subarray_with_sum_k([1,2,3,1,1,1,1,3,3], 6));//4
-console.log(longest_subarray_with_sum_k([1,2,3,1,1,1,1,3,19], 19));//1 changed while condition to n-1 to make sure loop doesnt run for right = 9
-console.log(longest_subarray_with_sum_k([19,2,3,1,1,1,1,3,1], 19));//0 incorrect //this is due to use never checking for 1st element we immediately sum it with next as it eneters the loop
+console.log(longest_subarray_with_sum_k([1,2,3,1,1,1,1,3,3], 6));//4
+console.log(longest_subarray_with_sum_k([1,2,3,1,1,1,1,3,19], 19));//1 
+console.log(longest_subarray_with_sum_k([19,2,3,1,1,1,1,3,1], 19));//1
