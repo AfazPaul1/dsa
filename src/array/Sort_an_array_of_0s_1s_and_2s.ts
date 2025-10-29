b8eb50cde9df5082feb31695d2b54a56d851a814
AfazPaul1 <afazpaul@gmail.com>
2025-10-29 10:56:58 +0530

    - sort_an_array_of_0s_1s_and_2s - used dutch national flag algorithm - 3 pointers follow 3 rules 0 to low -1 contains 0, low to mid-1 is 1's, mid to high is unsorted, high+1 to extremem right is 2's. based on which element mid is,  we swap with the corresponding bucket accordingly - tc: (n) cause in case of 0,1 we increment mid and in case of 2 we decrement high in anycase we are sorting 1 element and if every element was unsorted then in total we would be sorting N elements. - sc: O(1)

--- src/array/Sort_an_array_of_0s_1s_and_2s.ts
+++ src/array/Sort_an_array_of_0s_1s_and_2s.ts
@@ -1,24 +1,22 @@
+//dutch national flag
 function sort_an_array_of_0s_1s_and_2s(arr) {
-    let count0 = 0
-    let count1 = 0
-    let count2 = 0
-    for (let i = 0; i < arr.length; i++) {
-        if(arr[i] === 0) count0++
-        else if(arr[i] === 1) count1++
-        else count2++
-    }
-    for (let j = 0; j < count0; j++) {
-        arr[j] = 0
-    }
-    for (let k = count0; k < count1+count0; k++) {
-        arr[k] = 1
-    }
-    for (let l = count1+count0; l < arr.length; l++) {
-        arr[l] = 2
-        
+    let low = 0
+    let mid = 0
+    let high = arr.length-1
+    while(mid<=high) {
+        if (arr[mid] === 0) {
+            [arr[low], arr[mid]] = [arr[mid], arr[low]]
+              low++
+            mid++
+        } else if(arr[mid] === 1)  {
+            mid++
+        }else if(arr[mid] === 2) {
+            [arr[mid], arr[high]] = [arr[high], arr[mid]]
+            high--
+        }
     }
     return arr
+     
 }
-console.log(sort_an_array_of_0s_1s_and_2s([2,0,2,1,1,0]));
-
-
+console.log(sort_an_array_of_0s_1s_and_2s([0,1,1,0,1,2,1,2,0,0,0]));
+//console.log(sort_an_array_of_0s_1s_and_2s([2,0,2,1,1,0]));
