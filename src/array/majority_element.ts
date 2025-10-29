a5d524764930abbcd3853f3b88f6254daffc6630
AfazPaul1 <afazpaul@gmail.com>
2025-10-29 11:24:39 +0530

    - majority_element - while building the hash we simultaneously check whether the current element's frequence exceeds the half value. this reduces tc from 2N where we build hash and then iterate it to find max value to just N. but since its an unordered map at the worst case it could ne N*N - sc: worst case each  could be a unique element hence size of hash could be N hence (n)

--- src/array/majority_element.ts
+++ src/array/majority_element.ts
@@ -1,14 +1,11 @@
 function majority_element(arr:number[]) {
     let half = Math.floor(arr.length/2)
-    let count = 0
-    for (let i = 0; i < arr.length; i++) {
-        count = 0
-        for (let j = 0; j < arr.length; j++) {
-            if (arr[j] === arr[i]) {
-                count++
-            }
-        }
-        if(count > half) return arr[i] 
+    let hash =  new Map<number, number>()
+    for (const ele of arr) {
+        hash.set(ele, (hash.get(ele) || 0) + 1)
+        //console.log(hash);
+        if(hash.get(ele)! > half) return ele
     }
+    return hash 
 }
-console.log(majority_element([2,2,3,3,1,2,2]));
+console.log(majority_element([2,2,3,3,1,2,2,3,3,3,3,3,3]));
