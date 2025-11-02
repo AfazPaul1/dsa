d615c13e31646d37fc99b3076eb0238109b2973e
AfazPaul1 <afazpaul@gmail.com>
2025-11-03 00:39:49 +0530

    - max_subarray_sum - optimal using kadane - If a prefix sum becomes negative, any subarray starting before this point can’t possibly yield a larger sum than one starting after it - tc: O(n) - sc: O(1)

--- src/array/max_subarray_sum.ts
+++ src/array/max_subarray_sum.ts
@@ -1,12 +1,11 @@
 function max_subarray_sum(array) {
     let max= 0
-    for (let i = 0; i < array.length; i++) {
-        let sum = 0
-        for (let j = i; j < array.length; j++) {
-            sum+=array[j]
-            max = Math.max(sum, max)
-        }  
-    }
+    let sum = 0
+     for (let i = 0; i < array.length; i++) {
+        sum += array[i]
+        max = Math.max(sum, max)
+        if(sum < 0) sum = 0
+     }
     return max
 }
 console.log(max_subarray_sum([-2,-2,4,-1,-2,1,5,-3]));
