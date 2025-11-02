1c8a394cc5a767545e48e6aca0dab1d61f0e9245
AfazPaul1 <afazpaul@gmail.com>
2025-11-02 16:18:48 +0530

    - majority_element - optimal solution - tc: O(n) if there's a guaranteed majority element - O (2N) for another loop which count's the majority element's count and checks whether its greater than n/2 cause this approach works only if theres a majority element - constant space - If an element appears more than n/2 times, it can’t be completely canceled out — its frequency advantage guarantees it survives all pairings.

--- src/array/majority_element.ts
+++ src/array/majority_element.ts
@@ -1,11 +1,31 @@
+//most voting algorithm
 function majority_element(arr:number[]) {
-    let half = Math.floor(arr.length/2)
-    let hash =  new Map<number, number>()
-    for (const ele of arr) {
-        hash.set(ele, (hash.get(ele) || 0) + 1)
-        //console.log(hash);
-        if(hash.get(ele)! > half) return ele
+    let count = 0
+    let candidate = 0
+    let n = arr.length
+    for (let i = 0; i < arr.length; i++) {
+        if(count === 0) {
+            candidate = arr[i]
+            count=1
+        }
+        else if (arr[i] === candidate) {
+            count++
+        }
+        else {
+            count--
+        }
     }
-    return hash 
+    //only need this step if there isnt gauranteed to be a majority element
+    let count1 = 0
+    for (let j = 0; j < arr.length; j++) {
+        if(arr[j] === candidate) count1++
+    }
+    if(count1 > n/2) return candidate
+    else return -1
+    
 }
 console.log(majority_element([2,2,3,3,1,2,2,3,3,3,3,3,3]));
+console.log(majority_element([1,2,3,4,5,6]));
+console.log(majority_element([4,4,2,4,3,4,4,3,2,4]));
+console.log(majority_element([3,2,3]));
+
