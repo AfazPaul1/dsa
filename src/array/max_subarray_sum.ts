3fc4dd2a9f18a9be214e42a5973bbfdf0298bb5f
AfazPaul1 <afazpaul@gmail.com>
2025-11-03 01:08:18 +0530

    - max_subarray_sum - but print the subarray with the maximun sub too - decalre 2 variables to track the start and end index of the subarray. since they can only be continuous, we can slice then later this is easier than keeping track of all elements. whenever the sum is 0 that means the start of the subarray and whenever we set max to the current sum that means the subarray was extended to include the current element so we set endIndex to the current element when that happens. - this mostly works - tc: O(n) same

--- src/array/max_subarray_sum.ts
+++ src/array/max_subarray_sum.ts
@@ -1,11 +1,20 @@
 function max_subarray_sum(array) {
     let max= 0
     let sum = 0
+    let startI = 0
+    let endI = 0
      for (let i = 0; i < array.length; i++) {
+        if(sum === 0)  startI = i
         sum += array[i]
-        max = Math.max(sum, max)
+        if(sum > max ) {
+            max = sum
+            endI = i
+        }
         if(sum < 0) sum = 0
      }
-    return max
+    return [array.slice(startI, endI+1), max]
 }
-console.log(max_subarray_sum([-2,-2,4,-1,-2,1,5,-3]));
+//console.log(max_subarray_sum([-2,-2,4,-1,-2,1,5,-3])); //[ [ 4, -1, -2, 1, 5 ], 7 ]
+console.log(max_subarray_sum([-2,-2,4,-1,-2,1,5,-3, -5, -2,4,-1,-2,1,5,])); //[ [], 7 ]
+//the reason this wasnt working was at the end start index was 10, and endindex was 6 cause the max never exceeded earlier max but the sum became 0 hence the start index was changed and endindex never changed
+
