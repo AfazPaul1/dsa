02af407dd04912b475fe0982871fc71c3e9e7d2b
AfazPaul1 <afazpaul@gmail.com>
2025-11-03 16:55:44 +0530

    - best_time_to_buysell - optimal - O(n) - we didnt have to check for every previous element before the current selling price. we only needed the minimum among the elements we have seen which we can keep updating as we move through the array, like for a selling price of 3, and seen variables are [7,1,5] to find the max profit at that price we dont need to check 3-7, 3-5 cause no matter what value they are it will always yield a profit lower than what the minimum among them can yield suppose 1 here. - sc: constant - why dp? cause we are remembering the past which is the min here

--- src/dp/stock/best_time_to_buysell.ts
+++ src/dp/stock/best_time_to_buysell.ts
@@ -1,17 +1,17 @@
 function best_time_to_buysell(array) {
     let max_profit = 0
-    let buy = 0
+    let min = array[0];
     let sell = 0
-    for (let i = 0; i < array.length; i++) {
-        for (let j = 0; j <= i-1; j++) {
-            let cost = array[i] - array[j]
-            if( cost >  max_profit){
-                max_profit = cost
-                buy = array[j]
-                sell = array[i]
-            } 
+    for (let i = 1; i < array.length; i++) {
+        //min = Math.min(min, array[i])//we cant do this cause this would mean we are buying and selling on the same day at i=1 like when i = 1, array[1] which is 1 is our selling price if we immediately update min to 1 that would mean our buying price too is 1. only after we have processed it as a selling price do we consider it as a buying price. this doesnt apply to the first value in the array tho cause we have to buy to sell
+        let cost = array[i] - min
+        if( cost >  max_profit){
+            max_profit = cost
+            sell = array[i]
         }
+        min = Math.min(min, array[i])
     }
-    return [max_profit, buy, sell]
+    return [max_profit, min, sell]
 }
 console.log(best_time_to_buysell([7,1,5,3,6,4]));
+console.log(best_time_to_buysell([1, 7, 5,3,6,4]));
