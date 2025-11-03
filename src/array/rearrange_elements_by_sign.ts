248e7ff2305e2e0871e10e59f584136d029f55be
AfazPaul1 <afazpaul@gmail.com>
2025-11-03 13:15:51 +0530

    - rearrange_elements_by_sign - optimal O(n) - couldnt think further

--- src/array/rearrange_elements_by_sign.ts
+++ src/array/rearrange_elements_by_sign.ts
@@ -1,23 +1,17 @@
 function rearrange_elements_by_sign(array) {
-    let positives = []
-    let negatives  = []
-    for (let i = 0; i < array.length; i++) {
-        if(array[i] < 0) negatives.push(array[i])
-        else positives.push(array[i])
-    }
     let posI = 0
-    let negI = 0
-    for (let j = 0; j < array.length; j++) {
-        if(j%2 === 0) {
-            array[j] = positives[posI]
+    let negI = 1
+    let j = 0
+    while (j<array.length){
+        if(j%2 === 0 && array[j] < 0) {
+            
             posI++
-        } else {
-            array[j] = negatives[negI]
+        } else if (j%2 === 1 && array[j] > 0) {
+            
             negI++
         }
         
     }
     return array
-    
 }
 console.log(rearrange_elements_by_sign([3,1,-2,-5,2,-4]));//[ 3, -2, 1, -5, 2, -4 ]
