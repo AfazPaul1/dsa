1db241172354540d7e2311ab97a43fa61d43f92e
AfazPaul1 <afazpaul@gmail.com>
2025-11-04 12:35:44 +0530

    - all_permutations - same take not take backtracking algo - approach 1 - tc: n × (n−1) × (n−2) × ... × 1 = n! which is O(n*n!) - sc: O(n) auxiliary space & O(n*n!) n! permutations each of space n

---
+++ src/recursion/all_permutations.ts
@@ -0,0 +1,19 @@
+function all_permutations(array:number[], ds:number[], map: boolean[], res:number[][]) {
+    if(ds.length === array.length ) {
+        res.push([...ds])
+        return
+    }
+    for (let i = 0; i < array. length; i++) {
+        if(!map[i]) {
+            ds.push(array[i])
+            map[i] = true
+            all_permutations(array, ds, map, res)
+            ds.pop()
+            map[i] = false
+        }
+    }
+}
+const ans: number[][] = []
+all_permutations([1,2,3], [], [false, false, false], ans)
+console.log(ans);
+
