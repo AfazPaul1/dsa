b318ab482843d0c540f442a7c36b0fcd1eb347bb
AfazPaul1 <afazpaul@gmail.com>
2025-11-05 10:53:29 +0530

    - next_permutation - optimal - no extra space

--- src/array/next_permutation.ts
+++ src/array/next_permutation.ts
@@ -1,22 +1,27 @@
 function next_permutation(array:number[]) {
     let n = array.length
-    let idx = 0
+    let idx = -1
     for (let i = n-2; i >= 0; i--) {
         if (array[i] < array[i+1]) {
             idx = i
             break
         }  
     }
+    if(idx === -1) return array.reverse()
     for (let j = n-1; j >= idx; j--) {
         if (array[j] > array[idx]) {
             [array[idx], array[j]] = [array[j], array[idx]]
             break
         }  
     }
-    const newArr = array.splice(idx+1, n-1).reverse()
-    return [...array, ...newArr]
-
-  
+    let i = idx+1
+    let j = n-1
+    while(i < j) {
+        [array[i], array[j]] = [array[j], array[i]]
+        i++
+        j--
+    }
+    return array
 }
 //console.log(next_permutation([1,2,3]));//[ 1, 3, 2 ]
 console.log(next_permutation([2,1,5,4,3,0,0]));
