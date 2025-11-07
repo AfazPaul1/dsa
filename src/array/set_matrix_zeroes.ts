5b61ff54c605c1ea82be7e5a000cf1ae64b1f8cf
AfazPaul1 <afazpaul@gmail.com>
2025-11-07 16:15:26 +0530

    - set_matrix_zeroes - i was incorrectly using length of rows which is 3 instead of length of columns which is 4 - an optimization also the final 2 for loops rather than placing those if's inside it there by forcing the loop to run , we only need to run that loop if those conditions satisfy. its the same condition i dont know why i was checking it inside a loop lol - tc: 2*n*m - sc: constant O(1)

--- src/array/set_matrix_zeroes.ts
+++ src/array/set_matrix_zeroes.ts
@@ -21,8 +21,8 @@ function set_matrix_zeroes(array: number[][]) {
     }
     //here the issue is that we cant start from 0 cause thatll cause us to start changing our tracker array which might influence other trackers, like if we set arr[0][0] to 0 then that'll cause arr[0][3] to become zero cause its dependent on column 3 and row 0 and none of them contain original 0's so youll fall to the changed 0 trap too but if you look at the maxtrix you'll see that it should have remained a 1
     //but then how we change first row and column? its done separately
-    for (let i = 1; i < array.length; i++) {
-        for (let j = 1; j < array.length; j++) {
+    for (let i = 1; i < n; i++) {
+        for (let j = 1; j < m; j++) {
             //since we have used the first row and column to track we check for each element like for 2,3 we check whether 0,3 or 2,0 are 0 if they are we change this too to 0
             //we also have to track the edge case of col0, like if j = 0 we do not check 0,0 but col0
             if(array[0][j] === 0 || array[i][0] === 0) {
@@ -33,13 +33,13 @@ function set_matrix_zeroes(array: number[][]) {
     //also theres a case of what we change first? rows or colums? 
     //similar thing happens here if we change the column first where the row will mistaken it to be original 0 and change
     //also since col0 is stored separately we can safely change it after changing row
-    for (let j = 0; j < array.length; j++) {
-        if (array[0][0] === 0) {
+    if (array[0][0] === 0) {
+        for (let j = 0; j < m; j++) {
             array[0][j] = 0
         }
     }
-    for (let i = 0; i < array.length; i++) {
-        if (col0 === 0) {
+    if (col0 === 0) {
+        for (let i = 0; i < n; i++) {
             array[i][0] = 0
         }
     }
@@ -57,11 +57,11 @@ console.log(set_matrix_zeroes([
     [1,0,1],
     [1,1,1]
 ]));
-//[ 
-//[ 1, 0, 1 ], 
-//[ 0, 0, 0 ], 
-//[ 1, 0, 1 ] 
-//]
+[ 
+[ 1, 0, 1 ], 
+[ 0, 0, 0 ], 
+[ 1, 0, 1 ] 
+]
 console.log(set_matrix_zeroes([
     [0,1,2,0],
     [3,4,5,2],
