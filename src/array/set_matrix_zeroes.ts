e0e4195b8362210ae30bf5d748b9970ed15dc39c
AfazPaul1 <afazpaul@gmail.com>
2025-11-07 15:32:29 +0530

    - set_matrix_zeroes - optimal - no extra space we are using the first row and column to track those zeroes - incomplete - need to somehow remove the first row/column to zeros

--- src/array/set_matrix_zeroes.ts
+++ src/array/set_matrix_zeroes.ts
@@ -1,29 +1,41 @@
 function set_matrix_zeroes(array: number[][]) {
     let n = array.length
     let m = array[1].length
-    let infoRow = new Array(n).fill(0)
-    let infoCol = new Array(n).fill(0)
+    let col0 = 1
     for (let i = 0; i < array.length; i++) {
         for (let j = 0; j < array[i].length; j++) {
-            if (array[i][j] === 0) {
-                infoRow[i] = 1
-                infoCol[j] = 1           }
+            if(array[i][0] === 0) col0 = 0
+            else if(array[i][j] === 0) {
+                //we use these as the row and column arrays
+                //the problem we face here is array[0][0] will it refer to the row or column?
+                //also if it refers to the row where will we store the column tracker?
+                //we just use a extra variable for it
+                array[0][j] = 0
+                array[i][0] = 0
+
+            }
         }
     }
-    //console.log(infoCol, infoRow);
-    
-    for (let i = 0; i < array.length; i++) {
-        for (let j = 0; j < array[i].length; j++) {
-            if (infoRow[i] === 1 || infoCol[j] === 1) array[i][j] = 0   
-        } 
+    //here the issue is that we cant start from 0 cause thatll cause us to start changing our tracker array which might influence other trackers, like if we set arr[0][0] to 0 then that'll cause arr[0][3] to become zero cause its dependent on column 3 and row 0 and none of them contain original 0's so youll fall to the changed 0 trap too but if you look at the maxtrix you'll see that it should have remained a 1
+    //but then how we change first row and column? its done separately
+    for (let i = 1; i < array.length; i++) {
+        for (let j = 1; j < array.length; j++) {
+            //since we have used the first row and column to track we check for each element like for 2,3 we check whether 0,3 or 2,0 are 0 if they are we change this too to 0
+            //we also have to track the edge case of col0, like if j = 0 we do not check 0,0 but col0
+            //if()
+            if(array[0][j] === 0 || array[i][0] === 0) {
+                array[i][j] = 0
+            } 
+        }   
     }
+    
     return array
 }
 console.log(set_matrix_zeroes([
     [1,1,1,1],
     [1,0,1,1],
     [1,1,0,1],
-    [1,0,0,1]
+    [0,1,1,1]
 ]));
 console.log(set_matrix_zeroes([
     [1,1,1],
