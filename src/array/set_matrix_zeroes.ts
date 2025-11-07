cdbf8ab3d9b3fb71ec319aa11c8f81cda587647e
AfazPaul1 <afazpaul@gmail.com>
2025-11-07 12:15:49 +0530

    - set_matrix_zeroes - another way instea dof a tuple with true false is just mark it as another number like -1 and then do another loop after its done and convert them to 0s - to acces vertical column values just do a inner loop again

--- src/array/set_matrix_zeroes.ts
+++ src/array/set_matrix_zeroes.ts
@@ -2,13 +2,31 @@ function set_matrix_zeroes(array: number[][]) {
     for (let i = 0; i < array.length; i++) {
         for (let j = 0; j < array[i].length; j++) {
             if (array[i][j] === 0) {
-                array[i].map(ele => ele = 0)
+                for (let l = 0; l < array[i].length; l++) {
+                    if(array[i][l] !== 0) array[i][l] = -1
+                }
+                for (let k = 0; k < array.length; k++) {
+                    if(array[k][j] !== 0) array[k][j] = -1
+                }
             }
         }
     }
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array[i].length; j++) {
+            if (array[i][j] === -1) {
+                array[i][j] = 0
+            }   
+        } 
+    }
+    return array
 }
 console.log(set_matrix_zeroes([
     [1,1,1],
     [1,0,1],
     [1,1,1]
 ]));
+//[ 
+//[ 1, 0, 1 ], 
+//[ 0, 0, 0 ], 
+//[ 1, 0, 1 ] 
+//]
