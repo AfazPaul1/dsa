a2030c1c6371c5283b8c501f8dbcbe7124c2dedf
AfazPaul1 <afazpaul@gmail.com>
2025-11-08 14:16:48 +0530

    - spiral matric - ok it works - added a while loop which keeps executing the other 4 loops till exhaustion - also handled edge case where if there was only a single array or all array contain only single elements, like the bottom and left printing doesnt check whether such a bottom or left exists. cause in those cases top and bottom and left and right are the same thing so it just print the already printed thing again - tc: 1 iteration for every element hence O(n*m) - sc: ive logged here but i can store in array and i should hence thats another O(n*m)

--- src/array/spiral_matrix.ts
+++ src/array/spiral_matrix.ts
@@ -1,44 +1,52 @@
 function spiral_matrix(array: number[][]) {
     let left = 0
-    let right = 3
+    let right = array[0].length-1
     let top = 0
-    let bottom = 3
-    for (let i = left; i < array.length; i++) {
+    let bottom = array.length-1
+    while (top  <= bottom && left <= right) {
+        for (let i = left; i <= right; i++) {
         console.log(array[top][i]);
+        }
+        top++
+        for (let i = top; i <= bottom; i++) {
+            console.log(array[i][right]);        
+        }
+        right--
+        if (top <= bottom) {
+            for (let i = right; i >= left; i--) {
+                console.log(array[bottom][i]); 
+            }
+            bottom--
+        }
+        if (left <= right) {
+            for (let i = bottom; i >= top; i--) {
+                console.log(array[i][left]);   
+            }
+        left++
+        }
     }
-    top++
-    for (let i = top; i < array.length; i++) {
-        console.log(array[i][right]);
-    }
-    right--
-    for (let i = right; i >= left; i--) {
-        console.log(array[bottom][i]);   
-    }
-    bottom--;
-    for (let i = bottom; i >= top; i--) {
-        console.log(array[i][left]);
-    }
-    left++;
-    for (let i = left; i <= right; i++) {
-        console.log(array[top][i]);
-    }
-    top++
-    for (let i = top; i <= bottom; i++) {
-         console.log(array[i][right]);        
-    }
-    right--
-    for (let i = right; i >= left; i--) {
-        console.log(array[bottom][i]); 
-    }
-    bottom--
-    for (let i = bottom; i >= top; i--) {
-        console.log(array[i][left]);   
-    }
-    left++
+    
 }
+// console.log(spiral_matrix([
+//     [ 1, 2, 3, 4 ],
+// 	[ 5, 6, 7, 8 ],
+// 	[ 9, 10, 11, 12 ],
+// 	[ 13, 14, 15, 16 ]
+// ]));
+// console.log(spiral_matrix([
+//   [1, 2, 3, 4, 5, 6],
+//   [20, 21, 22, 23, 24, 7],
+//   [19, 32, 33, 34, 25, 8],
+//   [18, 31, 36, 35, 26, 9],
+//   [17, 30, 29, 28, 27, 10],
+//   [16, 15, 14, 13, 12, 11],
+// ]
+// ));
+console.log(spiral_matrix([[1,2,3,4,5,6]]));
 console.log(spiral_matrix([
-    [ 1, 2, 3, 4 ],
-	[ 5, 6, 7, 8 ],
-	[ 9, 10, 11, 12 ],
-	[ 13, 14, 15, 16 ]
-]));
+    [1],
+    [2],
+    [3],
+    [4]
+]));//1 2 3 4 3 2
+
