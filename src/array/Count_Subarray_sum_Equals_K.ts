609392f939b5094a6205ff932396ce4b93d326f5
AfazPaul1 <afazpaul@gmail.com>
2025-11-10 16:00:16 +0530

    - Count_Subarray_sum_Equals_K - variation which also returns the subarrays which sum to target

--- src/array/Count_Subarray_sum_Equals_K.ts
+++ src/array/Count_Subarray_sum_Equals_K.ts
@@ -1,15 +1,21 @@
 function Count_Subarray_sum_Equals_K(array: number[], k:number) {
     let count = 0
     let sum = 0
-    const hash =  new Map<number, number>()
-    hash.set(0,1)
+    const hash =  new Map<number, number[]>()
+    hash.set(0,[-1])
     for (let i = 0; i < array.length; i++) {
         sum+=array[i]
-        hash.set(sum, (hash.get(sum) || 0) + 1 )
+        let arr = hash.get(sum) || []
+        hash.set(sum, [...arr, i] )
         if(hash.has(sum-k)){
-            count+=hash.get(sum-k)!
+            for (const element of hash.get(sum-k)!) {
+                console.log(array.slice(element+1, i+1));
+            }
+            count+=hash.get(sum-k)!.length
         }
     }
+    //console.log(hash);
+    
     return count
 }
 console.log(Count_Subarray_sum_Equals_K([1, 2, 3, -3, 1, 1, 1, 4, 2, -3], 3)); 
