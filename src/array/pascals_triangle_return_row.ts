77832b48b987c5149b4a3544c16adb27dbe4af41
AfazPaul1 <afazpaul@gmail.com>
2025-11-10 16:38:48 +0530

    - pascals_triangle_return_row - optimal - follows a patern of accumulation like for 6 rows i=1 its value is given by 6-1/1 for i = 2 its existing * 6-2/2 and so on - tc: O(n) - sc: O(N)

--- src/array/pascals_triangle_return_row.ts
+++ src/array/pascals_triangle_return_row.ts
@@ -1,10 +1,14 @@
 import pascals_triangle_find_value_at_pos from "./pascals_triangle_find_value_at_pos";
 
 function pascals_triangle_return_row(row) {
-    let rowItems = []
-    for (let i = 0; i < row; i++) {
-        rowItems.push(pascals_triangle_find_value_at_pos(row-1,i))
+    let rowItems = [1]
+    let res = 1
+    for (let i = 1; i < row; i++) {
+        res*=row-i
+        res/=i
+        rowItems.push(res)
     }
     return rowItems
 }
 console.log(pascals_triangle_return_row(6)); //[ 1, 5, 10, 10, 5, 1 ]
+console.log(pascals_triangle_return_row(8));
