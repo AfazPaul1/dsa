ee21149a5d4f17e7c19f13e747675765dd51b2d2
AfazPaul1 <afazpaul@gmail.com>
2025-11-10 11:33:22 +0530

    -  Count_Subarray_sum_Equals_K - optimal solution - the intuition is that if the current sum from beg to now minus our target is a prefix sum we have already seen like suppose we have already seen sum 3 at 2 current sums and sum-k is our target then from that 2 current sum indices to now are 2 new subarrays whose sum is also 3 - explain deeply in notes - Every time a previous prefix sum = (current prefix sum − k), you automatically have one valid subarray ending at i - tc: O(n) cause only 1 run - sc: O(n) for storing the hash

--- src/array/Count_Subarray_sum_Equals_K.ts
+++ src/array/Count_Subarray_sum_Equals_K.ts
@@ -1,18 +1,15 @@
 function Count_Subarray_sum_Equals_K(array: number[], k:number) {
-    
     let count = 0
+    let sum = 0
+    const hash =  new Map<number, number>()
+    hash.set(0,1)
     for (let i = 0; i < array.length; i++) {
-        let sum = 0
-        for (let j = i; j < array.length; j++) {
-            sum+=array[j]
-            //if(sum > k) break oh looks like i cant do this cause of negatives
-            if(sum === k) count++ 
+        sum+=array[i]
+        hash.set(sum, (hash.get(sum) || 0) + 1 )
+        if(hash.has(sum-k)){
+            count+=hash.get(sum-k)!
         }
     }
     return count
 }
-console.log(Count_Subarray_sum_Equals_K([1, 2, 3, -3, 1, 1, 1, 4, 2, -3], 3));
-//it counts to 7 buts its wrong this happens because we are not counting the first element like suppose its a single first element 3 thats not being counted now cause we are addding to sum the next number without first checking
-//but moving the sum check before the sum operation gives 6 lol
-//ig me trying to start j from i+1 and initializing sum with the first element was issue
-//i undid all those and it worked
+console.log(Count_Subarray_sum_Equals_K([1, 2, 3, -3, 1, 1, 1, 4, 2, -3], 3)); 
