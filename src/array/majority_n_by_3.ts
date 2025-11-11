8a51dbef26a0cf69ff558c0f0dc839574bafc62a
AfazPaul1 <afazpaul@gmail.com>
2025-11-11 14:27:07 +0530

    - majority_n_by_3 -  some optimizations of the brute - not repeating for already counted numbers and suppose the second element is found early then return cause max there can only be 2 elements whose count is n/3

--- src/array/majority_n_by_3.ts
+++ src/array/majority_n_by_3.ts
@@ -2,6 +2,7 @@ function majority_n_by_3(array: number[]) {
     const n_by_3 = Math.floor(array.length/3)
     const res = new Set()
     for (let i = 0; i < array.length; i++) {
+        if(res.has(array[i])) continue
         let count = 0
         for (let j = 0; j < array.length; j++) {
             if(array[i] === array[j]) {
@@ -9,6 +10,7 @@ function majority_n_by_3(array: number[]) {
             }
         }
         if(count > n_by_3) res.add(array[i])
+        if(res.size === 2) return res
     }
 return res
 }
