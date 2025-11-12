e7a8a2f47025f42096e02d7e829a7494f0d22815
AfazPaul1 <afazpaul@gmail.com>
2025-11-12 13:14:26 +0530

    - three_sum - better solution - now i can reduce space complexity from 2n to n without result - tc: slightly increases due to nlogn sorting but nsquare still largely dominates - the key is to keep incrementing j and k using a while till a j or k value which is not equal to the current j or k value is found. since array is sorted if sum is less than 0 then we increment left pointer and if its greater then we decrement right pointer. if we find our combination we increment both till unique. also we use the ame string logic to check for duplicates - there might be a optimal O(1) space solution which ill explore next

--- src/array/3_sum.ts
+++ src/array/3_sum.ts
@@ -1,28 +1,65 @@
 function three_sum(array) {
     let seen = new Set()
     let triplets = []
+    array.sort((a,b) => a-b)
     for (let i = 0; i < array.length; i++) {
-        let hashSet = new Set()
-        for (let j = i+1; j < array.length; j++) {
-             let k = -(array[i] + array[j])
-             if(hashSet.has(k)) {
-                let triplet = [array[i], array[j], k].sort((a,b) => a-b)
-                let tripletStr = triplet.join(',')
+        let j = i+1
+        let k = array.length-1
+        while(j<k) {
+            if(array[i] + array[j] + array[k] < 0) {
+                j = incrJ(array, j)
+            } else if(array[i] + array[j] + array[k] > 0) {
+                k = decrK(array, k)
+            }
+            else  {
+                let triplet = [array[i], array[j] , array[k]]
+                let tripletStr = triplet.join(",")
                 if(!seen.has(tripletStr)) {
                     seen.add(tripletStr)
                     triplets.push(triplet)
                 }
-             }
-             hashSet.add(array[j])
+                //triplets.push([array[i], array[j] , array[k]]) cant do this anymore to ensure no duplicates
+                //forgot to do this causing an infinite loop where sum remained 0
+                //j++
+                //k--
+                //cant  do that that might just give the same elements hence same sum we need to incr/decr till unique
+                j = incrJ(array, j)
+                k = decrK(array, k)
+            }
         }
+        
     }
     return triplets
 }
+function incrJ(array, j) {
+    let jVal = array[j]
+    while(array[j] === jVal ) {
+        j++
+    }
+    return j
+}
+function decrK(array, k) {
+    let kVal = array[k]
+    while(array[k] === kVal ) {
+        k--
+    }
+    return k
+}
 console.log(three_sum([-1,0,1,2,-1,-4])); 
-//[ [ -1, 0, 1 ], [ -1, -1, 2 ], [ -4, 2, 2 ], [ -4, -1, 5 ] ]
-//initially got this
-//the reason for last one was i was adding k to hash set
-//the reason for 3rd is i wasnt resetting hashset
-//finally [ [ -1, -0, 1 ], [ -1, -1, 2 ] ]
 console.log(three_sum([-1,0,1,0])); //[ [ -1, -0, 1 ] ] in the exampole tho it shows duplicates like  [-1,1,0]  in the ans
-
+console.log(three_sum([-2, -2, -2, -1, -1, -1, 0, 0, 0, 2, 2, 2, 2]));
+// ok since sum is less than k i need to keep incremeenting j till i get a value that is not equal to the j in the sum we calculated
+// but how do i do this?
+//i could store current j in a new variable and use it for the while condition
+// also need to extract it into a function so it could be reused
+// this approach works for all 3 arrays
+//[ [ -2, 0, 2 ], [ -1, -1, 2 ], [ 0, 0, 0 ] ]
+//below is what i get without using seen
+//[
+//   [ -2, 0, 2 ],
+//   [ -2, 0, 2 ],
+//   [ -2, 0, 2 ],
+//   [ -1, -1, 2 ],
+//   [ -1, -1, 2 ],
+//   [ 0, 0, 0 ]
+// ]
