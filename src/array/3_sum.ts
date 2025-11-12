482dc0d1d0b2f149d87d20460621b96d666e3716
AfazPaul1 <afazpaul@gmail.com>
2025-11-12 14:55:31 +0530

    - three_sum - optimal O(1) sc approach - tc: O(n*n) + nlogn - using a simple if condition which checks whether this value of i is the same as before which means we already have the possible combination then we skip that i - this ensured we dont need to use seen - removed the use of those helper functions

--- src/array/3_sum.ts
+++ src/array/3_sum.ts
@@ -1,65 +1,31 @@
 function three_sum(array) {
-    let seen = new Set()
     let triplets = []
     array.sort((a,b) => a-b)
     for (let i = 0; i < array.length; i++) {
+        if (i > 0 && array[i] === array[i - 1]) continue
         let j = i+1
         let k = array.length-1
         while(j<k) {
-            if(array[i] + array[j] + array[k] < 0) {
-                j = incrJ(array, j)
-            } else if(array[i] + array[j] + array[k] > 0) {
-                k = decrK(array, k)
+            let sum = array[i] + array[j] + array[k]
+            if(sum < 0) {
+                j++ //logically this is correct, i cant skip all duplicates of j for all k values just cause a single k didnt match, but somehow i cant find a array to verify this and i get the corrcet answer irrespective
+            } else if(sum > 0) {
+                k--
             }
             else  {
                 let triplet = [array[i], array[j] , array[k]]
-                let tripletStr = triplet.join(",")
-                if(!seen.has(tripletStr)) {
-                    seen.add(tripletStr)
-                    triplets.push(triplet)
-                }
-                //triplets.push([array[i], array[j] , array[k]]) cant do this anymore to ensure no duplicates
-                //forgot to do this causing an infinite loop where sum remained 0
-                //j++
-                //k--
-                //cant  do that that might just give the same elements hence same sum we need to incr/decr till unique
-                j = incrJ(array, j)
-                k = decrK(array, k)
+                triplets.push(triplet)
+                //with the below 2 lines we ensure that j-1 exists and from there we check equivalency
+                j++
+                k--
+                while(j < k && array[j] === array[j-1]) j++
+                while(j < k && array[k] === array[k+1]) k--
             }
-        }
-        
+        }   
     }
     return triplets
 }
-function incrJ(array, j) {
-    let jVal = array[j]
-    while(array[j] === jVal ) {
-        j++
-    }
-    return j
-}
-function decrK(array, k) {
-    let kVal = array[k]
-    while(array[k] === kVal ) {
-        k--
-    }
-    return k
-}
 console.log(three_sum([-1,0,1,2,-1,-4])); 
-console.log(three_sum([-1,0,1,0])); //[ [ -1, -0, 1 ] ] in the exampole tho it shows duplicates like  [-1,1,0]  in the ans
+console.log(three_sum([-1,0,1,0]));
 console.log(three_sum([-2, -2, -2, -1, -1, -1, 0, 0, 0, 2, 2, 2, 2]));
-// ok since sum is less than k i need to keep incremeenting j till i get a value that is not equal to the j in the sum we calculated
-// but how do i do this?
-//i could store current j in a new variable and use it for the while condition
-// also need to extract it into a function so it could be reused
-// this approach works for all 3 arrays
-//[ [ -2, 0, 2 ], [ -1, -1, 2 ], [ 0, 0, 0 ] ]
-//below is what i get without using seen
-//[
-//   [ -2, 0, 2 ],
-//   [ -2, 0, 2 ],
-//   [ -2, 0, 2 ],
-//   [ -1, -1, 2 ],
-//   [ -1, -1, 2 ],
-//   [ 0, 0, 0 ]
-// ]
+console.log(three_sum([-2, -1, -1, -1, 3, 4]));
