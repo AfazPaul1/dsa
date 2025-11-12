67e8b8432cb1923b8ccd1ccfc20c96158916fe06
AfazPaul1 <afazpaul@gmail.com>
2025-11-12 12:00:31 +0530

    - three_sum - better solution - tc: n square and constant for unordered set - a+b+c = 0 and c = -(a+b) that is what we are doing here to reduce tc - we then verify that k is not our current i and j value using a hash and then do the usual uniqueness check - sc: 3n for need hashset, seen and triplets - i might be asked to get rid of seen - but i dont think i can do it in js might be possible in c++ by sorting the array initially and then insertion - maybe we can not use hashset - also triplets is for returning the answer and not used in the sum

--- src/array/3_sum.ts
+++ src/array/3_sum.ts
@@ -2,20 +2,27 @@ function three_sum(array) {
     let seen = new Set()
     let triplets = []
     for (let i = 0; i < array.length; i++) {
+        let hashSet = new Set()
         for (let j = i+1; j < array.length; j++) {
-            for (let k = j+1; k < array.length; k++) {
-                if(array[i] + array[j] + array[k] === 0) {
-                    let triplet = [array[i], array[j], array[k]].sort((a,b) => a-b)
-                    let tripletStr = triplet.join(',')
-                    if(!seen.has(tripletStr)) {
-                        seen.add(tripletStr)
-                        triplets.push(triplet)
-                    }
-                    
+             let k = -(array[i] + array[j])
+             if(hashSet.has(k)) {
+                let triplet = [array[i], array[j], k].sort((a,b) => a-b)
+                let tripletStr = triplet.join(',')
+                if(!seen.has(tripletStr)) {
+                    seen.add(tripletStr)
+                    triplets.push(triplet)
                 }
-            }
+             }
+             hashSet.add(array[j])
         }
     }
     return triplets
 }
-console.log(three_sum([-1,0,1,2,-1,-4]));
+console.log(three_sum([-1,0,1,2,-1,-4])); 
+//[ [ -1, 0, 1 ], [ -1, -1, 2 ], [ -4, 2, 2 ], [ -4, -1, 5 ] ]
+//initially got this
+//the reason for last one was i was adding k to hash set
+//the reason for 3rd is i wasnt resetting hashset
+//finally [ [ -1, -0, 1 ], [ -1, -1, 2 ] ]
+console.log(three_sum([-1,0,1,0])); //[ [ -1, -0, 1 ] ] in the exampole tho it shows duplicates like  [-1,1,0]  in the ans
+
