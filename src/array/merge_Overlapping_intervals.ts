666fa7e62082f1ca9c3d122f8db389f0c13663ba
AfazPaul1 <afazpaul@gmail.com>
2025-11-13 15:03:48 +0530

    - merge_Overlapping_intervals - i was pushing incorrectly leading to other ways to compensate - was blindly reassigning the second variable to the next item's second value when i should have been comparing them both and have the max among them as second.like [15,19] and [16,17] without max checking would insert [15,17] when it should have been [15,19] - i was wrong about tc, even though for most i the inner loop doesnt execute it still counts so outer runs for n times, and the inner runs for n times too cause it never decreases like, if j was 3 when i was 0 then i fails next few times, and when i is finally true and its 3, j will be 4 so in total it'll only run n times - nlogn tfor sorting - tc: nlogn + 2n - need more optimal

--- src/array/merge_Overlapping_intervals.ts
+++ src/array/merge_Overlapping_intervals.ts
@@ -9,31 +9,38 @@ function merge_Overlapping_intervals(array) {
         return a.length-b.length
     }
     )
-    //ok so how do i loop this
-    //first when i start for 1st element and go through at some point when i doesnt overlap i break out and dont check anymore, i also dont start from the immediate next element but from where i left off cause they are already part of a sequence so no use starting from them again
-    //how how do i loop this in a for every thing is looped but we dont want that maybe i could skip yes 
     let res = []
     for (let i = 0; i < array.length; i++) {  
         let first = array[i][0]
         let second = array[i][1]
         if(res.length && second <= res[res.length-1][1] ) continue
-        if(!array[i+1]) {
-            res.push([first, second])
-            break
-        }
+        // if(!array[i+1]) {
+        //     res.push([first, second])
+        //     break
+        // } //can remove this now 
         for (let j = i+1; j < array.length; j++) {
-            if(second > array[j][0]) {
-                second = array[j][1]
+            if(second >= array[j][0]) {
+                second = Math.max(array[j][1], second)
+                //max cause when its [15,18] & [16,17] we cant have our ebtry be [15,17] when it should be [15,18]
             } else {
-                res.push([first, second])
+                //res.push([first, second]) //[ [ 1, 6 ], [ 8, 11 ], [ 16, 17 ] ]
+                //having it here results in this array which is wrongs the numbers are in range 15-18
+                //when i is at 6 and j is at 7 theres no next element
+                //hence j cant be 8 and exits the loop
+                //our updated second value was 18 and first was 15
+                //but it never got inserted cause j became 8 and else never executed
+                //to handle this i had a special insertion in i loop but that created a new first and second and inserted it which are not values we have validated nor updated which resulted in 16,17 inserted 
+                //that is why we need our push after the j for loop
+                //though which we can avoid the need for the special inserion
                 break
             }
         }
+        res.push([first, second])
     }
     return res
     
 }
-console.log(merge_Overlapping_intervals([[8,10],[15,18], [1,3],[2,6],]));
+//console.log(merge_Overlapping_intervals([[8,10],[15,18], [1,3],[2,6],]));
 //[ [ 1, 6 ], [ 8, 10 ], [ 15, 18 ] ]
 console.log(merge_Overlapping_intervals([
   [1, 3],
@@ -45,5 +52,4 @@ console.log(merge_Overlapping_intervals([
   [15, 18],
   [16, 17]
 ]));
-//[ [ 1, 6 ], [ 8, 11 ], [ 16, 17 ] ] when it should have been [15,18] as the 3rd element and not [16,17]
-
+//[ [ 1, 6 ], [ 8, 11 ], [ 15, 18 ] ]
