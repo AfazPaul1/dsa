adb2cf44eab95359221cae4ab727009db8c5d43c
AfazPaul1 <afazpaul@gmail.com>
2025-11-13 12:50:29 +0530

    - merge_Overlapping_intervals - i think through wrong this solution is O(n) - sc: just to return res which can be O(n) otherwise constant. we also sort inplace -

---
+++ src/array/merge_Overlapping_intervals.ts
@@ -0,0 +1,52 @@
+function merge_Overlapping_intervals(array) {
+    array.sort((a, b) => {
+        let len = Math.min(a.length, b.length)
+        for (let i = 0; i < len; i++) {
+            if(a[i] !== b[i]) {
+                return a[i] - b[i]
+            }   
+        }
+        return a.length-b.length
+    }
+    )
+    //ok so how do i loop this
+    //first when i start for 1st element and go through at some point when i doesnt overlap i break out and dont check anymore, i also dont start from the immediate next element but from where i left off cause they are already part of a sequence so no use starting from them again
+    //how how do i loop this in a for every thing is looped but we dont want that maybe i could skip yes 
+    let res = []
+    let newStart = -1
+    for (let i = 0; i < array.length; i++) {
+        if(i < newStart) continue
+        let first = array[i][0]
+        let second = array[i][1]
+        if(!array[i+1]) {
+            res.push([first, second])
+            break
+        }
+        for (let j = i+1; j < array.length; j++) {
+            
+            if(second > array[j][0]) {
+                second = array[j][1]
+            } else {
+                newStart = j
+                res.push([first, second])
+                break
+            }
+        }
+    }
+    return res
+    
+}
+console.log(merge_Overlapping_intervals([[8,10],[15,18], [1,3],[2,6],]));
+//[ [ 1, 6 ], [ 8, 10 ], [ 15, 18 ] ]
+console.log(merge_Overlapping_intervals([
+  [1, 3],
+  [2, 6],
+  [8, 9],
+  [9, 11],
+  [8, 10],
+  [2, 4],
+  [15, 18],
+  [16, 17]
+]));
+//[ [ 1, 6 ], [ 8, 11 ], [ 16, 17 ] ] when it should have been [15,18] as the 3rd element and not [16,17]
+
