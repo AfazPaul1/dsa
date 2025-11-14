696bc439281ac0fe24cf7fe51985f462dc079a88
AfazPaul1 <afazpaul@gmail.com>
2025-11-14 15:41:33 +0530

    - merge_two_sorted_arrays - am not handling cases where the pointers could be in the second array. -  extracted the swap into a seprate function. - handle the i and j values passed to it correctly - also the reason the gap check for 1 is placed there is cause we want it to execute once - tc: the while loop depends on the gap value. it keeps being halved hence log2(n+m). for each value of gap then inner loop runs nearly n+m times. hence  - O(log2(n+m))*O((n+m)) -sc: constant

--- src/array/merge_two_sorted_arrays.ts
+++ src/array/merge_two_sorted_arrays.ts
@@ -3,29 +3,28 @@ function merge_two_sorted_arrays(arr1, arr2) {
     let m = arr2.length
     let len = n+m
     let gap = Math.ceil((len)/2)
-    
+    function swapIfGreater(array1:number[], array2:number[], left:number, right:number ) {
+        if(arr1[left] > arr2[right]) {
+            [arr1[left],  arr2[right]] = [arr2[right], arr1[left]]            
+        }
+    }
     
     while(gap > 0) {
         let i = 0
         let j = i + gap
         while(j < len) {
-            if(j >= n) {
-                if(arr1[i] > arr2[j-n]) {
-                    [arr1[i],  arr2[j-n]] = [arr2[j-n], arr1[i]]
-                    
-                }
-                i++
-                    j++
-            } else {
-                if(arr1[i] > arr1[j]) {
-                    [arr1[i],  arr1[j]] = [arr1[j], arr1[i]]
-                    
-                }
-                i++
-                    j++
+            if(j >= n && i < n) {
+                swapIfGreater(arr1,arr2, i, j-n)
+            } else if(i>= n) {
+                swapIfGreater(arr2,arr2, i-n, j-n)
             }
+            else {
+                swapIfGreater(arr1,arr1, i, j)
+            }
+            i++
+            j++
         }
-        if(gap === 1) return [arr1, arr2]
+        if(gap === 1) break
         gap = Math.ceil(gap/2)
         
     }
