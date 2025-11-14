ac31928a2fd4b3c70e7d3b0377bf4b07535a1837
AfazPaul1 <afazpaul@gmail.com>
2025-11-14 22:58:14 +0530

    - missing_and_repeating_number - just solving quadratic equations - first find sums of the 2 and then find squares of them - finally solve them and we'll get the answers - tc: O(2n) for 2 reduces i guess i can convert it intoa  single loop so - tc: O(n) - sc: constant

--- src/array/missing_and_repeating_number.ts
+++ src/array/missing_and_repeating_number.ts
@@ -1,17 +1,29 @@
 function missing_and_repeating_number(array ) {
     let k = array.length
-    let missing = -1
-    let repeating = -1
-    let hash = new Map()
-    for (const element of array) {
-        hash.set(element, (hash.get(element) | 0) + 1)
+    let s = (k*(k+1))/2
+    let sq = ((k)*(k+1)*((2*k)+1))/6
+    // let sn = array.reduce((acc, cur) => acc+cur)
+    // let sqn = array.reduce((acc, cur) => acc+(cur*cur), 0)//need to give intial as 0 else 3 will be initial
+    let sn = 0
+    let sqn = 0
+    for (let i = 0; i < array.length; i++) {
+        sn+=array[i]
+        sqn+=array[i]*array[i]
     }
-    for (let i = 1; i <= k; i++) {
-        if(!hash.has(i)) missing = i
-        else if(hash.get(i) === 2) repeating = i
-        else if(missing !== -1 && repeating !== -1) break //meaning we have found them both so no need to check more hence break
-    }
-    console.log(hash);
-    return [missing, repeating]
+    let h = s-sn //x-y
+
+    let m = sq-sqn //x²+y² = (x+y)(x-y)
+    let g = m/h //(x+y) = (x²+y²)/(x-y)
+    //how do i do quadratic equations here
+    //ok so i would have to preclculate the remaining values and then find it
+    //like 
+    //x+y=g
+    //+
+    //x-y=h
+    //2x = g+h
+    let x = (g+h)/2
+    let y = g-x
+    //but like how do i know which is which?
+    return [x, y]
 }
 console.log(missing_and_repeating_number([3,1,2,5,3]));
