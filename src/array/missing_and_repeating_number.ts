f95188884113d9fbb026a449381a42f31f5ce243
AfazPaul1 <afazpaul@gmail.com>
2025-11-14 18:53:05 +0530

    -  missing_and_repeating_number - better solution using hashing - 3 loops one runs k times, another array.size times and another hash.size times so around 3n - tc: O(3n)

--- src/array/missing_and_repeating_number.ts
+++ src/array/missing_and_repeating_number.ts
@@ -1,13 +1,21 @@
 function missing_and_repeating_number(array, k) {
     let missing = -1
     let repeating = -1
-    for (let i = 1; i < k; i++) {
-        let count = 0
-        for (let j = 0; j < array.length; j++) {
-            if(array[j] === i) count++
-        }
-        if(count === 0) missing = i
-        else if(count === 2)  repeating = i
+    let hash = new Map()
+    //ok so how do i do this like if i loop through array and build hash i can only find the repeated and not the missing cause it wont have an entry
+    //so some way i can loop through the k and hash
+    //like i need atleast a 2n solution
+    //maybe i should first try with 3n solution
+    for (let i = 1; i <= k; i++) {
+        hash.set(i, 0)
+    }
+    for (const element of array) {
+        hash.set(element, (hash.get(element) | 0) + 1)
+    }
+    console.log(hash);
+    for (const [element, value] of hash) {
+        if(value === 2) repeating = element
+        else if(value === 0) missing = element
     }
     return [missing, repeating]
 }
