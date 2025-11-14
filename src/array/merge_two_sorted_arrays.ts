58e854e4ac055f39022376463f8f6ddbfa76520b
AfazPaul1 <afazpaul@gmail.com>
2025-11-14 13:05:01 +0530

    - merge_two_sorted_arrays - this condition is not robust it only works cause its js - like consider second run, first i will be 1 and j will be zero condition will be true cause 20 > 1 and they will swap and j will be 1 and i will 0. arr2[j] doesnt exist, its undefined so we are comparing whether arr1[0] > undefined which it always will be. this is just a js coincidence and not how clean code should be written

--- src/array/merge_two_sorted_arrays.ts
+++ src/array/merge_two_sorted_arrays.ts
@@ -1,33 +1,17 @@
 function merge_two_sorted_arrays(arr1, arr2) {
-    let i = 0
-    let j = 0
-    let arr3= []
     let n = arr1.length
     let m = arr2.length
-    while( i < n && j < m){
-        if(arr1[i] > arr2[j]) {
-            arr3.push(arr2[j])
-            j++
-        } else if(arr1[i] < arr2[j]) {
-            arr3.push(arr1[i])
-            i++
-        }
-    }
-    while(i < n) {
-        arr3.push(arr1[i])
-        i++
-    }
-    while(j < m) {
-        arr3.push(arr2[j])
+    let i = n-1
+    let j = 0
+    
+    while(arr1[i] > arr2[j]) {
+        [arr1[i], arr2[j]] = [arr2[j], arr1[i]]
+        i--
         j++
     }
-    for (let k = 0; k < arr3.length; k++) {
-        if(k >= n)  {
-            arr2[k-n] = arr3[k] //k-n instead of using another variable like start j from 0 and increment separately
-        } else {
-            arr1[k] = arr3[k]
-        }
-    }
+    arr1.sort((a,b) => a-b)
+    arr2.sort((a,b) => a-b)
     return [arr1, arr2]
 }
 console.log(merge_two_sorted_arrays([1,3,5,7], [0,2,6,8,9]));
+console.log(merge_two_sorted_arrays([10,20], [1]));
