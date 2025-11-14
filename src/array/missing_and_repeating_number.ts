6ee84452b4068183d266c03fba2bc7da76d84ab9
AfazPaul1 <afazpaul@gmail.com>
2025-11-14 19:04:28 +0530

    -  missing_and_repeating_number - better solution - first i should just record presence - record presence and then check for which i to k the hash doesnt have it that is your missing number - tc: O(2n) - sc: n for the hash

--- src/array/missing_and_repeating_number.ts
+++ src/array/missing_and_repeating_number.ts
@@ -1,22 +1,17 @@
-function missing_and_repeating_number(array, k) {
+function missing_and_repeating_number(array ) {
+    let k = array.length
     let missing = -1
     let repeating = -1
     let hash = new Map()
-    //ok so how do i do this like if i loop through array and build hash i can only find the repeated and not the missing cause it wont have an entry
-    //so some way i can loop through the k and hash
-    //like i need atleast a 2n solution
-    //maybe i should first try with 3n solution
-    for (let i = 1; i <= k; i++) {
-        hash.set(i, 0)
-    }
     for (const element of array) {
         hash.set(element, (hash.get(element) | 0) + 1)
     }
-    console.log(hash);
-    for (const [element, value] of hash) {
-        if(value === 2) repeating = element
-        else if(value === 0) missing = element
+    for (let i = 1; i <= k; i++) {
+        if(!hash.has(i)) missing = i
+        else if(hash.get(i) === 2) repeating = i
+        else if(missing !== -1 && repeating !== -1) break //meaning we have found them both so no need to check more hence break
     }
+    console.log(hash);
     return [missing, repeating]
 }
-console.log(missing_and_repeating_number([3,1,2,5,3], 5));
+console.log(missing_and_repeating_number([3,1,2,5,3]));
