622fd9c6ca76caedbf4fda5216de89ed1905fd9e
AfazPaul1 <afazpaul@gmail.com>
2025-11-14 15:03:47 +0530

    - merge_two_sorted_arrays - optimal solution 2 - gap method from shell sort

--- src/array/merge_two_sorted_arrays.ts
+++ src/array/merge_two_sorted_arrays.ts
@@ -1,21 +1,34 @@
 function merge_two_sorted_arrays(arr1, arr2) {
     let n = arr1.length
     let m = arr2.length
-    let i = n-1
-    let j = 0
+    let len = n+m
+    let gap = Math.ceil((len)/2)
     
-    while(i >= 0 && j < m) {
-        if(arr1[i] > arr2[j]) {
-            [arr1[i], arr2[j]] = [arr2[j], arr1[i]]
-            i--
-            j++
-        } else {
-            break
+    
+    while(gap > 0) {
+        let i = 0
+        let j = i + gap
+        while(j < len) {
+            if(j >= n) {
+                if(arr1[i] > arr2[j-n]) {
+                    [arr1[i],  arr2[j-n]] = [arr2[j-n], arr1[i]]
+                    
+                }
+                i++
+                    j++
+            } else {
+                if(arr1[i] > arr1[j]) {
+                    [arr1[i],  arr1[j]] = [arr1[j], arr1[i]]
+                    
+                }
+                i++
+                    j++
+            }
         }
+        if(gap === 1) return [arr1, arr2]
+        gap = Math.ceil(gap/2)
         
     }
-    arr1.sort((a,b) => a-b)
-    arr2.sort((a,b) => a-b)
     return [arr1, arr2]
 }
 console.log(merge_two_sorted_arrays([1,3,5,7], [0,2,6,8,9]));
