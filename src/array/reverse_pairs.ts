62a9f3e5134bbd5f878e8928e97f77118c5e7d43
AfazPaul1 <afazpaul@gmail.com>
2025-11-15 20:21:53 +0530

    - reverse_pairs - optimal - tc: logn for merge sort - n for the merge operation - the reverse pairs is similar to 2 arrays hence n1+n2 which is the same array broken into 2 hence about n. - total tc: 2nlogn - sc: O(n) and we are distorting the array - we will have 2 sorted arrays for each element from left on we find till where there are elements whose twice is still less than the left one and till that we move right then we add from mid to that right to our count

--- src/array/reverse_pairs.ts
+++ src/array/reverse_pairs.ts
@@ -1,15 +1,55 @@
 function reverse_pairs(array: number[]) {
-    let res = []
-    let count = 0
-    for (let i = 0; i < array.length; i++) {
-        for (let j = i+1; j < array.length; j++) {
-            if(array[i] > 2*array[j]){
-                res.push([array[i], array[j]])
-                count++
-            }
-        }
-    }
-    return count
+    return mergeSort(array, 0, array.length-1)
 }
 console.log(reverse_pairs([1,3,2,3,1]));//2
 console.log(reverse_pairs([3,2,1,4]));//1
+console.log(reverse_pairs([40, 25, 19, 12, 9,6, 2]));//1
+function mergeSort(array:number[], low:number, high:number) {
+    let count = 0
+    if (low === high) {
+        return count
+    }
+    let mid = Math.floor((low+high)/2)
+    count+=mergeSort(array, low, mid)
+    count+=mergeSort(array, mid+1, high)
+    count+=findReversePairs(array, low, mid, high)
+    merge(array, low, mid, high)
+    return count
+}
+function merge(array:number[], low:number, mid:number, high:number) {
+    const temp = []
+    let left = low
+    let right = mid+1
+    while (left <= mid && right <= high) {
+        if (array[left] < array[right]) {
+            temp.push(array[left])
+            left++
+        } else {
+            temp.push(array[right])
+            right++
+        }
+    }
+    while (left <= mid) {
+        temp.push(array[left])
+        left++
+    }
+    while (right <=high) {
+        temp.push(array[right])
+        right++
+    }
+    for (let i = 0; i < temp.length; i++) {
+        array[low] = temp[i] 
+        low++
+    }   
+}
+function findReversePairs(array, low, mid, high) {
+    let count1 = 0
+    let right = mid+1
+    for (let left = low; left <= mid; left++) {
+        while(right <= high && (2*array[right]) < array[left]  ){
+            right++ 
+        }  
+        count1+=right-(mid+1)
+    }
+    return count1
+}
