de55e8f94e85bffb0a7243e429a84e3f307f77b2
AfazPaul1 <afazpaul@gmail.com>
2025-11-15 18:07:05 +0530

    - Count_inversions_in_an_array - keeping track of the count is the harder thing here - like for [2] [1] in the merge we track that 1 separately and then return it ad then add it to the existing count. - for [4,5] [3] [4,3] & [5,3] are the pairs. we need to increment count by 2 but how? left and low will be 0, mid is 1, high is 2  its from (mid-left+1) ie from the current element till mid is sorted so if left is bigger than right then everything from left to mid will be bigger. +1 to include both ends -tc: merge sort  is nlogn hence this too is nlogn - sc: O(n) for temp - am altering the input array so if im asked not to ill work with a copy

--- src/array/Count_inversions_in_an_array.ts
+++ src/array/Count_inversions_in_an_array.ts
@@ -1,13 +1,49 @@
-function Count_inversions_in_an_array(array) {
+
+function mergeSort1(array, low, high) {
     let count = 0
-    for (let i = 0; i < array.length; i++) {
-        for (let j = i+1; j < array.length; j++) {
-            if(array[j] < array[i]) {
-                count++
-            }
+    if(low === high) return count
+    
+    let mid = Math.floor((low+high)/2)
+    count+=mergeSort1(array, low, mid)
+    count+=mergeSort1(array, mid+1, high)
+    count+= merge(array, low, mid, high)
+    return count
+}
+ 
+function merge(array, low, mid, high) {
+    let left = low
+    let right = mid+1
+    let temp = []
+    let count1 = 0
+    while(left <= mid && right <= high) {
+        if(array[left] <= array[right]){
+            temp.push(array[left])
+            left++
+        } else {
+            
+            temp.push(array[right])
+            count1+=(mid-left+1)
+            right++
         }
     }
-    return count
+    while(left <= mid){
+        temp.push(array[left])
+        left++
+    }
+    while(right <= high){
+        temp.push(array[right])
+        right++
+    }
+    for (let i = 0; i < temp.length; i++) {
+        array[low] = temp[i]
+        low++  
+    }
+    return count1
+}
+function Count_inversions_in_an_array(array) {
+    
+    let fiCount = mergeSort1(array, 0, array.length-1)
+    return [array, fiCount]
 }
 console.log(Count_inversions_in_an_array([5,4,3,2,1]));
 console.log(Count_inversions_in_an_array([5,3,2,1,4]));
