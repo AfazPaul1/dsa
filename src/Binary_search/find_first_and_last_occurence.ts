9fc2cc5772fc883ea6eddf3ad2cd77c3775c4fcc
AfazPaul1 <afazpaul@gmail.com>
2025-11-17 23:32:34 +0530

    - find_first_and_last_occurence - the first occurance could only be the lower bound and the last occurance could be the element before the upper bound - we just check whether it indeed is the target in case it doesnt exists in the array ta all - tc: same 2*log_2n

--- src/Binary_search/find_first_and_last_occurence.ts
+++ src/Binary_search/find_first_and_last_occurence.ts
@@ -1,14 +1,44 @@
-function find_first_and_last_occurence(array, target) {
-    let first  = -1
-    let last = -1
-    for (let i = 0; i < array.length; i++) {
-        if(array[i] === target) {
-            if(first === -1) first  = i
-            last = i
+function find_first_occurence(array, target) {
+    let first  = array.length
+    let low = 0
+    let high = array.length-1
+    while(low <= high) {
+        let mid = Math.floor((low+high)/2)
+        if(array[mid] >= target) {
+            first = mid
+            high = mid-1
+        } else {
+            low = mid+1
         }
-        
     }
-    return [first, last]
+    return first
 }
-console.log(find_first_and_last_occurence([3,4,13,13,13,20,40], 13));
-console.log(find_first_and_last_occurence([3,4,13,13,13,20,40], 40));
+function find_last_occurence(array, target) {
+    let last = array.length
+    let low = 0
+    let high = array.length-1
+    while(low <= high) {
+        let mid = Math.floor((low+high)/2)
+        if(array[mid] > target) {
+            last = mid
+            high = mid-1
+        } else {
+            low = mid+1
+        }
+    }
+    return last
+}
+function find_first_and_last_occurence(array, target) {
+    let n = array.length
+    let first = find_first_occurence(array, target)
+    let last = find_last_occurence(array, target)
+    //these are 2 edge cases like if lb was never found hence its value would be n or lb is found but its not the target cause the targest does not exist
+    if(first === n || array[first] !== target) return -1
+    return [first, last-1]
+
+}
+console.log(find_first_and_last_occurence([3,4,13,13,13,20,40], 13));//[2,4]
+console.log(find_first_and_last_occurence([3,4,13,13,13,20,40], 40));//[ 6, 6 ]
+console.log(find_first_and_last_occurence([3,4,13,13,13,20,40, 41], 40));//[ 6, 6 ]
+console.log(find_first_and_last_occurence([3,4,13,13,13,20,40], 60));//-1
+
