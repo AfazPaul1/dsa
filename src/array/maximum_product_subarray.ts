a0e01154f641e1c603d4ce9993e44c53e6b73b76
AfazPaul1 <afazpaul@gmail.com>
2025-11-17 10:47:42 +0530

    - maximum_product_subarray - optimal - tc: O(n) - If the overall array product is positive (even number of negatives), the prefix will capture it on the last iteration. - If the overall array product is negative (odd number of negatives), the maximum product subarray is formed by dropping either the first or the last negative element. The prefix sweep implicitly checks the result of dropping the last negative, while the suffix sweep implicitly checks the result of dropping the first negative.

--- src/array/maximum_product_subarray.ts
+++ src/array/maximum_product_subarray.ts
@@ -1,12 +1,19 @@
 function maximum_product_subarray(array) {
+    let n = array.length
     let max  = 0
-    for (let i = 0; i < array.length; i++) {
-        let product = 1
-        for (let j = i; j < array.length ; j++) {
-            product*=array[j]
-            max = Math.max(product, max)
-        }
+    let prefix = 1
+    let suffix = 1
+    for (let i = 0; i < n; i++) {    
+        if(suffix === 0) suffix = 1
+        if(prefix === 0) prefix = 1
+        prefix*=array[i]
+        suffix*=array[n-i-1]
+        max = Math.max(max, Math.max(prefix, suffix))
     }
     return max
 }
-console.log(maximum_product_subarray([1,2,3,4,5,0]));//120
+//console.log(maximum_product_subarray([1,2,3,4,5,0]));//120
+//console.log(maximum_product_subarray([1,2,-3,0,-4,-5]));//20
+//console.log(maximum_product_subarray([2,3,-2,4]));//6
+//console.log(maximum_product_subarray([-2,3,4,-1,0,-2,3,1,4,0,4,6,-1,4]));//24
+console.log(maximum_product_subarray([3,4,-1,-2,3,1,4,4,6,-1,4]));
