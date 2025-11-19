690f143c75ce4aa52f11189652fe816a0d865452
AfazPaul1 <afazpaul@gmail.com>
2025-11-19 22:34:26 +0530

    - single_element_sorted_array - consolidate those cases into a single condition

--- src/Binary_search/single_element_sorted_array.ts
+++ src/Binary_search/single_element_sorted_array.ts
@@ -10,23 +10,14 @@ function single_element_sorted_array(array) {
         if(array[mid] !== array[mid-1] && array[mid] !== array[mid+1]){
             return array[mid]
         }
-        //even odd or odd even pattern so how do i check for this condition
-        //so i find whether mid is even or odd and check whether right or left is same as mid. if mid is odd and left is same then even odd pattern then single is in the other part
-        //ok but how do i code it
-        //odd
-        if(mid%2 === 1) {
-            //even odd
-            if(array[mid] === array[mid-1])  low = mid+1
-            //oddeven
-            else high = mid-1
+        if(mid%2 === 1 && array[mid] === array[mid-1] || mid%2 === 0 && array[mid] === array[mid+1]) {
+            low = mid+1
         } 
-        //even
-        else if(mid%2 === 0) {
-            //odd even
-            if(array[mid] === array[mid-1])  high = mid-1
-            else low = mid+1
+        else  {
+            high = mid-1
         }
     }
+    return -1
 }
 console.log(single_element_sorted_array([1,1,2,2,3,3,4,5,5,6,6]));
 console.log(single_element_sorted_array([1,1,3,5,5]));
