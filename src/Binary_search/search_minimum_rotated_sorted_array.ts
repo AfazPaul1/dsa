e514e3ebbec8f5aa95866cf3d0f5b95ac25d5865
AfazPaul1 <afazpaul@gmail.com>
2025-11-19 12:57:28 +0530

    - search_minimum_rotated_sorted_array - take the smallest from the sorted part and eliminate that part just keep doing this and when loop exhausts we will have our minimum

---
+++ src/Binary_search/search_minimum_rotated_sorted_array.ts
@@ -0,0 +1,20 @@
+function search_minimum_rotated_sorted_array(array) {
+    let n = array.length
+    let low = 0
+    let high = n-1
+    let ans = Infinity // one three special numeric constants its type will be number 
+    //earlier i used -1 but then i cant set minimum cause -1 will always be lesser 
+    //ans === -1 ? ans = array[low] : ans = Math.min(array[low], ans) so i used something like this which doesnt workfor negatives?
+    while(low <= high) {
+        let mid = Math.floor((low+high) /2 )
+        if(array[low] <= array[mid]) {
+            ans = Math.min(array[low], ans)
+            low = mid+1
+        } else {
+            ans = Math.min(array[mid], ans)
+            high=mid-1
+        }
+    }
+    return ans  
+}
+console.log(search_minimum_rotated_sorted_array([4,5,6,7,0,1,2,3]));
