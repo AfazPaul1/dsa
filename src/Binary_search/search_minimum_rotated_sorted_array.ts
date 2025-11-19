f64c920282132643d7acd36d7ae280b808deea62
AfazPaul1 <afazpaul@gmail.com>
2025-11-19 16:14:01 +0530

    - search_minimum_rotated_sorted_array - handle case where low  = mid = high - this low we are skipping over could be our answer too so we update min with it too - in this case the tc could worsen to O(n/2)

--- src/Binary_search/search_minimum_rotated_sorted_array.ts
+++ src/Binary_search/search_minimum_rotated_sorted_array.ts
@@ -2,16 +2,18 @@ function search_minimum_rotated_sorted_array(array) {
     let n = array.length
     let low = 0
     let high = n-1
-    let ans = Infinity // one three special numeric constants its type will be number 
-    //earlier i used -1 but then i cant set minimum cause -1 will always be lesser 
-    //ans === -1 ? ans = array[low] : ans = Math.min(array[low], ans) so i used something like this which doesnt workfor negatives?
+    let ans = Infinity 
     while(low <= high) {
         let mid = Math.floor((low+high) /2 )
+        if(array[low] === array[mid] && array[mid] === array[high]) {
+            ans = Math.min(array[low], ans) //need to do this or we might skip 1
+            low++
+            high--
+            continue
+        }
         if(array[low] <= array[high]) {
             ans = Math.min(array[low], ans)
             break
-            //this happens in the eg: 1 array
-            //return array[low] //still have to compare
         }
         if(array[low] <= array[mid]) {
             ans = Math.min(array[low], ans)
@@ -23,6 +25,7 @@ function search_minimum_rotated_sorted_array(array) {
     }
     return ans  
 }
-console.log(search_minimum_rotated_sorted_array([4,5,6,7,0,1,2,3]));//0
-console.log(search_minimum_rotated_sorted_array([7, 8, 1, 2, 3, 3, 3, 4, 5, 6]));//1 this still retuns correctly without any code to handle duplictaes? ah probably because they occur a=on the right sorted part and are eliminates so never looked at. oh i missed a point here cause duplicates only become an issue when low  = mid  = high
+//console.log(search_minimum_rotated_sorted_array([4,5,6,7,0,1,2,3]));//0
+//console.log(search_minimum_rotated_sorted_array([7, 8, 1, 2, 3, 3, 3, 4, 5, 6]));//1 this still retuns correctly without any code to handle duplictaes? ah probably because they occur a=on the right sorted part and are eliminates so never looked at. oh i missed a point here cause duplicates only become an issue when low  = mid  = high
 console.log(search_minimum_rotated_sorted_array([2, 2, 2, 2, 3, 1, 2]));//we get 2 when it should have been 1
+//1 is set when low = mid = high = 5
