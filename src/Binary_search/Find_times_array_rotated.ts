a33762f0f7f5108039a0cbb8555fba93dcc1f45c
AfazPaul1 <afazpaul@gmail.com>
2025-11-19 16:17:54 +0530

    - Find_times_array_rotated - this problem is an extension of minimum in a sorted rotated array since the minimum would have been at 0 index and index it is currently at would be the number of places it has been rotated - the min only stores the value - so i use another varible which keeps track of the index of the min. maybe there might be a better way to do this lets see - also we can also extend this to handle duplicates too

---
+++ src/Binary_search/Find_times_array_rotated.ts
@@ -0,0 +1,21 @@
+function Find_times_array_rotated(array) {
+    let n = array.length
+    let low = 0
+    let high = n-1
+    let min = Infinity
+    let ans = -1
+    while(low <= high) {
+        let mid = Math.floor((low+high)/2)
+        if(array[low] <= array[mid]){
+            min = Math.min(min, array[low])
+            if(min === array[low]) ans = low
+            low = mid+1
+        } else {
+            min = Math.min(min, array[mid])
+            if(min === array[mid]) ans = mid
+            high = mid-1
+        }
+    }
+    return ans
+}
+console.log(Find_times_array_rotated( [4,5,6,7,0,1,2,3]));
