66f74c95751cc457f84b0728a6e3711390924e2a
AfazPaul1 <afazpaul@gmail.com>
2025-11-20 15:29:47 +0530

    - find_sqrt - at the end of it all high will point to the last possible value so we return it - thorugh if high always stays at or before the true answer and through else high becomes the last place where mid was valid eventually high stays on the largest mid that was valid thus high = the best ie largest valid candidate so far - always use bs when we are  sure about the answer range - this is an important pattern where we know a range of answers and asked to find min or max in it - tc: log_2n

--- src/Binary_search/find_sqrt.ts
+++ src/Binary_search/find_sqrt.ts
@@ -1,15 +1,12 @@
 function find_sqrt(N:number) {
-    //like i cant use a for loop cause i would have to know how long it would run for
-    //but here we dont know that //ok actually there is a max value which we can use to loop which is the number itself
-    //so we keep incrementing and if at some value the square becomes bigger than n we exit
-    let cand = 1
-    let sq = 1
-    while(sq <= N) {
-        cand++
-        sq = cand*cand
+    let low = 0
+    let high = N
+    while(low <= high) {
+        let mid = Math.floor((low+high) /2)
+        if((mid*mid) > N) high = mid-1
+        else low = mid+1
     }
-    return cand-1
-    
+    return high
 }
 console.log(find_sqrt(36));//6
 console.log(find_sqrt(28));//5
