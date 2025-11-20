a20fb8ad1e72cacbfd87ef664ecbe37148c19cc5
AfazPaul1 <afazpaul@gmail.com>
2025-11-20 12:25:56 +0530

    - find_peak_element - only one peak - to avoid extra conditions inside the while where we would have to check whether its the last or first before doing mid-1 or mid+1 we reduce the search space and handle those cases separately - log_2n - its is kinda sorted and there a peak O(n) solution not accepted then bs is the option

--- src/Binary_search/find_peak_element.ts
+++ src/Binary_search/find_peak_element.ts
@@ -1,12 +1,19 @@
 function find_peak_element(array) {
     let n = array.length
-    for (let i = 0; i < array.length; i++) {
-        if((i === 0 || array[i-1] < array[i]) && (i === n-1 || array[i+1] < array[i])) //i wasnt bracketing properly initially hence it broke like it did (a || b && c || d ) when it should have been ((a || b) && (c || d) )
-        {
-            return i
-        }
+    let low = 1
+    let high = n-2
+    if(n === 1) return 0
+    if(array[0] > array[1]) return 0
+    if(array[n-1] > array[n-2]) return n-1
+    while(low <= high) {
+        let mid = Math.floor((low+high) /2)
+        if(array[mid] > array[mid-1] && array[mid] > array[mid+1]) return mid
+        else if(array[mid] > array[mid-1]) low=mid+1
+        //if checks where both are true if that if fails then one or both must have been wrong? i dont think both can we wrong since slope at least one condition must be true cause only 1 peak? if multiple peaks mid might be lesser than both left and right
+        //we check which is true by first checking a single condition individually if that turns true run that code        //if that turns false then the other must have been true 
+        else high = mid-1
     }
     return -1
 }
 console.log(find_peak_element([1,2,3,4,5,6,7,8,5,1]));//7
-console.log(find_peak_element([1,2,1,3,5,6,4]));//1
+console.log(find_peak_element([1,2,1,3,5,6,4]));//5// 2 peaks here 1 and 5 brute returned 1 and this return 5
