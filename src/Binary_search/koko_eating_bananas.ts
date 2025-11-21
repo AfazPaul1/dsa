8c876c25121aeecf7f0188ec60b3174e130ac6f8
AfazPaul1 <afazpaul@gmail.com>
2025-11-21 13:21:03 +0530

    - koko_eating_bananas - alt using a for with predictable tc - tc: O(max(arr)*lenofarray)

--- src/Binary_search/koko_eating_bananas.ts
+++ src/Binary_search/koko_eating_bananas.ts
@@ -1,15 +1,13 @@
 function koko_eating_bananas(arr, h) {
-    //totalhours
-    let k = Infinity
-    //bananas per hour
-    let hoursCandidate = 1
-    while(k > h) {
-        k = 0
+    // why max of array? because it can only 1 bucket in one hour cause we take ceil value and there are 4 buckets the minimum hours taken cannot be low than 4. the maximum bananas in a basket is 11 so max possible banans it can eat per hour is 11. even if it eats 12 or 20 0r 100 banans per hour it will still take 4 hours. so that is our max possible banans per hour value
+    let k = Math.max(...arr)
+    for (let i = 1; i <= k; i++) { //iterator is the candidate
+        let sum = 0
         for (const element of arr) {
-            k+=Math.ceil((element/hoursCandidate))
+            sum += Math.ceil(element/i)
         }
-        hoursCandidate++
+        if(sum <= h) return i
     }
-    return hoursCandidate-1
+    return -1
 }
 console.log(koko_eating_bananas([3,6,7,11], 8));
