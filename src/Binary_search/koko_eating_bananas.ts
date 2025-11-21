7c2221d1729f788914cfbd72de7061c72a069a11
AfazPaul1 <afazpaul@gmail.com>
2025-11-21 15:34:13 +0530

    - koko_eating_bananas optimal bs aproach - we will have a range of answers we will be searching in - if the sum we get from mid is greater than our required (meaning not possible ans) then any number lesser will yield a greater sum hence we can eliminate the left part. - if sum is equal or lesser then its a potential answer we store mid and trim right cause they will result in bigger answer but we want the minimum. - we need the minimum of possible values - tc: O(log_2 (max of array) *  (length of array) )

--- src/Binary_search/koko_eating_bananas.ts
+++ src/Binary_search/koko_eating_bananas.ts
@@ -1,13 +1,17 @@
 function koko_eating_bananas(arr, h) {
-    // why max of array? because it can only 1 bucket in one hour cause we take ceil value and there are 4 buckets the minimum hours taken cannot be low than 4. the maximum bananas in a basket is 11 so max possible banans it can eat per hour is 11. even if it eats 12 or 20 0r 100 banans per hour it will still take 4 hours. so that is our max possible banans per hour value
-    let k = Math.max(...arr)
-    for (let i = 1; i <= k; i++) { //iterator is the candidate
+    arr.sort((a,b) => a-b)
+    let low = 1
+    let high = Math.max(...arr)
+    while(low <= high) {
+        let mid = Math.floor((low+high)/2)
         let sum = 0
         for (const element of arr) {
-            sum += Math.ceil(element/i)
+            sum += Math.ceil(element/mid)
         }
-        if(sum <= h) return i
+        if(sum <= h) high = mid-1 //all possible values so we handle the equal case here
+        else if(sum > h) low = mid+1
     }
-    return -1
+    return low
 }
-console.log(koko_eating_bananas([3,6,7,11], 8));
+//console.log(koko_eating_bananas([3,6,7,11], 8));//4
+console.log(koko_eating_bananas([25, 12, 8, 14, 19], 5));//25
